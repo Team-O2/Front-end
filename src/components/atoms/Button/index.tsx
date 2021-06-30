68233e66f4404a984eae2bb7a32ed1e450df1286
Subin <sbsoobin@naver.com>
2021-07-01 03:02:00 +0900

    Feat: 헤더 뷰 구현

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -11,6 +11,9 @@ function Button({ children, ...props }: IProps): React.ReactElement {
   return <SButton {...props}>{children}</SButton>;
 }
 
-const SButton = Styled.button``;
+const SButton = Styled.button`
+    background : none;
+    border : none;
+  `;
 
 export default Button;
