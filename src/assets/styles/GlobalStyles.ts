b653a41b2b61a3e2acaf555c8da6be62c6fa88b6
YeongWoooo <rdd9223@gmail.com>
2021-06-30 19:31:39 +0900

    Env: 전역 색상, 컬러 설정

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,13 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
-import { theme } from 'styled-tools';
+import '../fonts/index.css';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
   
   * {
     margin: 0;
-    font-family: 'Spoqa Han Sans', 'Noto Sans KR', 'Gothic A1', sans-serif;
+    font-family: 'AppleSDGothicNeo';
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -22,33 +22,9 @@ const GlobalStyles = createGlobalStyle`
     text-decoration: none;
     cursor: pointer;
   }
-
-  h1 {
-    ${theme('fontStyle.h3')}
-  }
-
-  h2 {
-    ${theme('fontStyle.h4')}
-  }
-
-  h2 {
-    ${theme('fontStyle.h5')}
-  }
-
-  h3 {
-    ${theme('fontStyle.subtitle1')}
-  }
-
-  h4 {
-    ${theme('fontStyle.subtitle2')}
-  }
-
-  h5 {
-    ${theme('fontStyle.body1')}
-  }
-
-  p {
-    ${theme('fontStyle.body2')}
+  
+  @media (max-width: 1200px) {
+    padding-right: 1.8vw;
   }
 `;
 
--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,16 +1,29 @@
 const colorStyles = {
-  iconRed: '#FF0000',
-  contentCategory: '#88BDB7',
-  folincategory: '#00C2BC',
-  colorEmerald: '#00554A',
+  'colors-bg-f-3-fcff-light-bg-1': '#f3fcff',
+  'colors-bg-f-0-f-7-ff': '#f0f7ff',
+  'colors-bg-f-3-ffff-light-bg-2': '#ddfbfa',
+  'colors-bg-f-0-fafb': '#f0fafb',
+  'colors-primary-13-e-2-dd-o-2-mint': '#13e2dd',
+  'colors-primary-04-d-6-e-0': '#04d6e0',
+  'colors-primary-36-c-8-f-5-o-2-blue': '#36c8f5',
+  'colors-primary-58-e-2-ff': '#58e2ff',
+  'colors-primary-03-b-6-ce': '#03b6ce',
+  'colors-primary-00-baf-4': '#00baf4',
+  'colors-primary-f-66436-red-orange': '#f66436',
+  'colors-primary-e-04747-red': '#e04747',
 };
 
 const text = {
-  textBlack: '#2E2926',
-  textGray1: '#595959',
-  textGray2: '#9D9D9D',
-  textWhite: '#FFFFFF',
-  textGreen: '#1E9285',
+  'colors-grayscale-ff': '#ffffff',
+  'colors-grayscale-df-light-gray-1': '#dfdfdf',
+  'colors-grayscale-c-1-light-gray-2': '#c1c1c1',
+  'colors-grayscale-a-5': '#a5a5a5',
+  'colors-grayscale-8-b-gray': '#8b8b8b',
+  'colors-grayscale-6-f': '#6f6f6f',
+  'colors-grayscale-55': '#555555',
+  'colors-grayscale-3-d-dark-gray': '#3d3d3d',
+  'colors-grayscale-24': '#242424',
+  'colors-grayscale-0-d-black': '#0d0d0d',
 };
 
 const theme = {
