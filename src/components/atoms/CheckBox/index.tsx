468f0d330e14bfd4ea27c97b9f355b8479c06f02
YeongWoooo <rdd9223@gmail.com>
2021-07-01 18:23:15 +0900

    Feat: 체크박스 atom 구현

--- src/components/atoms/CheckBox/index.tsx
+++ src/components/atoms/CheckBox/index.tsx
@@ -1,1 +1,17 @@
-export {};
+import React from 'react';
+import Styled from 'styled-components';
+
+export interface IProps {
+  checked: boolean;
+  id?: string;
+  className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>, isChecked?: boolean) => void;
+}
+
+function CheckBox({ checked, ...props }: IProps): React.ReactElement {
+  return <SCheckBox type="checkbox" checked={checked} {...props} />;
+}
+
+const SCheckBox = Styled.input``;
+
+export default CheckBox;
