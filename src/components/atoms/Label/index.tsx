8d3de18335ac345d8490b5bd566680ba976b019e
YeongWoooo <rdd9223@gmail.com>
2021-07-01 18:26:03 +0900

    Feat: 라벨 atom 구현

--- src/components/atoms/Label/index.tsx
+++ src/components/atoms/Label/index.tsx
@@ -1,1 +1,15 @@
-export {};
+import React from 'react';
+import Styled from 'styled-components';
+
+export interface IProps {
+  name: string;
+  className?: string;
+}
+
+function Label({ name, ...props }: IProps): React.ReactElement {
+  return <SLabel {...props}>{name}</SLabel>;
+}
+
+const SLabel = Styled.label``;
+
+export default Label;
