41b2ae9303487cc27ab51c44d134e10f242565b5
YeongWoooo <rdd9223@gmail.com>
2021-07-01 18:25:06 +0900

    Feat: 인풋 atom 구현

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,1 +1,23 @@
-export {};
+import React from 'react';
+import Styled from 'styled-components';
+
+export interface IProps {
+  name: string;
+  value?: string | number;
+  invalid?: boolean;
+  disabled?: boolean;
+  className?: string;
+  defaultValue?: string;
+  placeholder?: string;
+  autoComplete?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocusOut?: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+function Input({ name, autoComplete = 'off', ...props }: IProps): React.ReactElement {
+  return <SInput name={name} autoComplete={autoComplete} {...props} />;
+}
+
+const SInput = Styled.input``;
+
+export default Input;
