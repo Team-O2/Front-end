e761fd9767a1cf39243bdf55e6644c643868e697
YeongWoooo <rdd9223@gmail.com>
2021-07-01 18:22:27 +0900

    Feat: 버튼 atom 생성

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,1 +1,16 @@
-export {};
+import React from 'react';
+import Styled from 'styled-components';
+
+export interface IProps {
+  children: React.ReactElement | string;
+  onClick?: () => void;
+  className?: string;
+}
+
+function Button({ children, ...props }: IProps): React.ReactElement {
+  return <SButton {...props}>{children}</SButton>;
+}
+
+const SButton = Styled.button``;
+
+export default Button;
