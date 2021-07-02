207b5aa3f9413cf8b3ac8f65c6ef110fbd2ec83f
YeongWoooo <rdd9223@gmail.com>
2021-07-03 03:24:10 +0900

    Feat: 링크 atom 구현

---
+++ src/components/atoms/Link/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Styled from 'styled-components';
+import { Link as link } from 'react-router-dom';
+
+export interface IProps {
+  children: string | React.ReactElement;
+  to: string;
+}
+
+function Link({ children, to, ...props }: IProps): React.ReactElement {
+  return (
+    <SLink to={to} {...props}>
+      {children}
+    </SLink>
+  );
+}
+
+const SLink = Styled(link)`
+`;
+
+export default Link;
