edeaf058a9ef2951e46efb01eb4ebed5dec502ab
JiyoungHyun <shyunji023@gmail.com>
2021-07-02 21:15:29 +0900

    Feat: ConcertDetailTitle molecules 구현

---
+++ src/components/templates/ShareTogetherDetail/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Styled from 'styled-components';
+import ConcertDetailTitle from '../../molecules/ConcertDetailTitle';
+import ConcertList from '../../organisms/ConcertList';
+
+interface IProps {
+  selectedConcert: any;
+}
+
+function ShareTogetherDetail({ selectedConcert }: IProps): React.ReactElement {
+  const { title, speaker, date, category } = selectedConcert;
+  return (
+    <SShareTogetherDetail>
+      <ConcertDetailTitle title={title} speaker={speaker} date={date} category={category}></ConcertDetailTitle>
+    </SShareTogetherDetail>
+  );
+}
+
+const SShareTogetherDetail = Styled.div`
+  width: 845px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export default ShareTogetherDetail;
