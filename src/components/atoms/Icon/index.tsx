8844981d2f0585d23b50e49ba7c7ea91333bdaf9
YeongWoooo <rdd9223@gmail.com>
2021-07-02 16:22:49 +0900

    Feat: 아이콘 atom추가

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -1,1 +1,21 @@
-export {};
+import React from 'react';
+import Styled from 'styled-components';
+import { ifProp } from 'styled-tools';
+
+export interface IProps {
+  alt: string;
+  src: string;
+  height?: string;
+  circular?: boolean;
+}
+
+function Icon({ height = '2rem', alt = '', ...props }: IProps): React.ReactElement {
+  return <SImg height={height} alt={alt} {...props} />;
+}
+
+const SImg = Styled.img`
+  height: ${(props) => props.height};
+  border-radius: ${ifProp('circular', '50%', '')}
+`;
+
+export default Icon;
