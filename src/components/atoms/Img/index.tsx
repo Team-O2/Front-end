da80552394d3180945abb13d4e044cde35dca817
YeongWoooo <rdd9223@gmail.com>
2021-07-04 02:01:50 +0900

    Refactor: 외부 굴곡 부모 스타일 적용, alt값 필수로 변경

--- src/components/atoms/Img/index.tsx
+++ src/components/atoms/Img/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Styled from 'styled-components';
 
 export interface IProps {
-  alt: string;
+  alt?: string;
   src: string;
 }
 
@@ -14,6 +14,7 @@ const SImg = Styled.img`
   width: inherit;
   height: inherit;
   object-fit: inherit;
+  border-radius: inherit;
 `;
 
 export default Img;
