04f72fd9e40138bebd2b57d42ae47d620623675f
YeongWoooo <rdd9223@gmail.com>
2021-07-04 02:11:27 +0900

    Refactor: palette를 이용한 색깔 선택 가능

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styled from 'styled-components';
+import { palette } from 'styled-tools';
 
 export interface IProps {
   name: string;
@@ -11,10 +12,16 @@ function Tag({ name, ...props }: IProps): React.ReactElement {
   return <STag {...props}>{name}</STag>;
 }
 
-const STag = Styled.span`
+interface ITagProps {
+  color?: string;
+}
+
+const STag = Styled.span<ITagProps>`
   display: inline-block;
-  border: 1px solid;
-  padding: 1.2rem 3rem;
+  border: 1px solid ${(props) => palette('primary', props.color)};
+  color: ${(props) => palette('primary', props.color)};
+  padding: 0.5rem 1.5rem;
+  /* padding: 1.2rem 3rem; (회원가입 태그)*/
   border-radius: 60px;
   text-align: center;
 `;
