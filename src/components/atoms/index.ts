445ea18e0de665c7d7b75a2bcba567333dacbe34
YeongWoooo <rdd9223@gmail.com>
2021-07-04 02:11:43 +0900

    Etc: Atoms 인덱싱

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,9 @@
+export { default as Button } from './Button';
+export { default as CheckBox } from './CheckBox';
+export { default as Icon } from './Icon';
+export { default as Img } from './Img';
+export { default as ImgSelector } from './ImgSelector';
+export { default as Input } from './Input';
+export { default as Link } from './Link';
+export { default as Label } from './Label';
+export { default as Tag } from './Tag';
