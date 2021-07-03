cb4cb059526e161cab12d37eb66294933915042d
YeongWoooo <rdd9223@gmail.com>
2021-07-04 04:58:13 +0900

    Refactor: className을 props로 받도록 변경

--- src/components/atoms/Img/index.tsx
+++ src/components/atoms/Img/index.tsx
@@ -4,10 +4,11 @@ import Styled from 'styled-components';
 export interface IProps {
   alt?: string;
   src: string;
+  className?: string;
 }
 
-function Img({ alt = '', src }: IProps): React.ReactElement {
-  return <SImg src={src} alt={alt} />;
+function Img({ alt = '', src, ...props }: IProps): React.ReactElement {
+  return <SImg src={src} alt={alt} {...props} />;
 }
 
 const SImg = Styled.img`
