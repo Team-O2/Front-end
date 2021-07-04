61cace61218c9b45ac2fe3b5cc02037353d5a8d0
YeongWoooo <rdd9223@gmail.com>
2021-07-04 17:04:06 +0900

    Style: 이미지 path가 비어있다면 배경색을 표시하도록 변경

--- src/components/atoms/Img/index.tsx
+++ src/components/atoms/Img/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Styled from 'styled-components';
+import { palette } from 'styled-tools';
 
 export interface IProps {
   alt?: string;
-  src: string;
+  src?: string;
   className?: string;
 }
 
 function Img({ alt = '', src, ...props }: IProps): React.ReactElement {
+  if (!src) {
+    return <SDiv {...props} />;
+  }
+
   return <SImg src={src} alt={alt} {...props} />;
 }
 
@@ -16,6 +21,14 @@ const SImg = Styled.img`
   height: inherit;
   object-fit: inherit;
   border-radius: inherit;
+`;
+
+const SDiv = Styled.div`
+  width: inherit;
+  height: inherit;
+  object-fit: inherit;
+  border-radius: inherit;
+  background-color: ${palette('emptyImage')};
 `;
 
 export default Img;
