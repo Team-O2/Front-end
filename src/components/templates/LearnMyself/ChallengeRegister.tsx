b541bede1c1d8ae1b9863f02719e699da55d81a1
Hyoin-Kim <hi980506@naver.com>
2021-07-10 16:23:50 +0900

    Feat : ChallengeRegister Page 구현

---
+++ src/components/templates/LearnMyself/ChallengeRegister.tsx
@@ -0,0 +1,12 @@
+import Register from 'components/molecules/ChallengeRegister';
+import React from 'react';
+
+const ChallengeRegister = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default ChallengeRegister;
