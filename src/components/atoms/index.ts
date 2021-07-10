1f66364d36bdae8df298c573e6e30036157014c5
YeongWoooo <rdd9223@gmail.com>
2021-07-10 15:25:26 +0900

    Style: export 순서 변경

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,7 +5,7 @@ export { default as Icon } from './Icon';
 export { default as Img } from './Img';
 export { default as ImgSelector } from './ImgSelector';
 export { default as Input } from './Input';
+export { default as Label } from './Label';
 export { default as Link } from './Link';
 export { default as MyPageCard } from './MyPageCard';
-export { default as Label } from './Label';
 export { default as Tag } from './Tag';
