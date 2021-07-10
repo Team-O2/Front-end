bd67a4b9f67996f7259605ed78646472975fc04a
Hyoin-Kim <hi980506@naver.com>
2021-07-10 16:37:26 +0900

    Feat : ChallengeList 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/index';
 import Login from './pages/Login/index';
+import WriteLearnMyself from 'components/templates/LearnMyself/ChallengeWrite';
+import LearnMyself from 'components/templates/LearnMyself/ChallengeList';
+import EditLearnMyself from 'components/templates/LearnMyself/ChallengeEdit';
+import ChallengeRegister from 'components/templates/LearnMyself/ChallengeRegister';
 
 function Router(): React.ReactElement {
   return (
@@ -9,6 +13,10 @@ function Router(): React.ReactElement {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Login" component={Login} />
+        <Route exact path="/challenge" component={LearnMyself} />
+        <Route exact path="/challengeRegister" component={ChallengeRegister} />
+        <Route exact path="/write" component={WriteLearnMyself} />
+        <Route exact path="/edit" component={WriteLearnMyself} />
         <Route component={() => <div>404 Not Found</div>} />
       </Switch>
     </BrowserRouter>
