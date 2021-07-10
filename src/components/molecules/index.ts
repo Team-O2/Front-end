40be5d9be8967f13cf9792d23f322769be8268b8
YeongWoooo <rdd9223@gmail.com>
2021-07-10 15:38:41 +0900

    Etc: 파일 인덱싱

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,5 @@
+export { default as CommentedBoardRow } from './CommentedBoardRow';
+export { default as LearnMyselfCard } from './LearnMyselfCard';
+export { default as MyPageSection } from './MyPageSection';
+export { default as MyPageSlider } from './MyPageSlider';
+export { default as ShareTogetherCard } from './ShareTogetherCard';
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,2 @@
+export { default as MyCommentList } from './MyCommentList';
+export { default as MyPageHeader } from './MyPageHeader';
