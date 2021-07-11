68866473148f0eae6f341609abff4e55297595a9
Subin <sbsoobin@naver.com>
2021-07-12 00:10:19 +0900

    Feat: 로그인 api 호출

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,6 +1,24 @@
 import axios from 'axios';
 
 export const serverAxios = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
+  // baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
+  baseURL: 'http://3.37.218.69:5000',
   withCredentials: true,
 });
+
+interface ILoginData {
+  email: string;
+  password: string;
+}
+
+export const postLogin = async (loginData: ILoginData) => {
+  try {
+    console.log('data', loginData);
+    const data = await serverAxios.post('/auth/signin', loginData);
+    console.log('[SUCCESS] POST host data');
+    return data;
+  } catch (e) {
+    console.log('[FAIL] POST host data', e);
+    return null;
+  }
+};
