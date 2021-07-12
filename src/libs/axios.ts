3aea29bd290b6521865a370f2b584068e73d0ded
Subin <sbsoobin@naver.com>
2021-07-12 20:56:33 +0900

    Feat: 챌린지 오픈 api연결

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -32,6 +32,19 @@ interface INoticeData {
   interest: string[];
   hashtag: string[];
 }
+interface IChallengeOpenData {
+  title: string;
+  challengePeriod: {
+    start: string;
+    end: string;
+  };
+  applyPeriod: {
+    start: string;
+    end: string;
+  };
+  peopleNum: number;
+  img: File | null;
+}
 
 export const postLogin = async (loginData: ILoginData) => {
   try {
@@ -135,6 +148,36 @@ export const postNoticeWrite = async (token: string, noticeWriteData: INoticeDat
   form.append('text', noticeWriteData.text);
   form.append('interest', `[${noticeWriteData.interest.join()}]`);
   form.append('hashtag', `[${noticeWriteData.hashtag.join()}]`);
+  try {
+    const data = await serverAxios.post('/admin/concert', {
+      headers: {
+        Authorization: token,
+      },
+      body: {
+        form,
+      },
+    });
+    console.log(data);
+  } catch (e) {
+    console.log(e.response.data);
+  }
+};
+const changeDateStyle = (date: string) => {
+  const year = date.substr(0, 4);
+  const month = date.substr(5, 2);
+  const day = date.substr(8, 2);
+  return `${year}-${month}-${day}`;
+};
+export const challengeOpen = async (token: string, challengeOpenData: IChallengeOpenData) => {
+  const form = new FormData();
+  form.append('title', challengeOpenData.title);
+  form.append('registerStartDT', changeDateStyle(challengeOpenData.applyPeriod.start));
+  form.append('registerEndDT', changeDateStyle(challengeOpenData.applyPeriod.end));
+  form.append('challengeStartDT', changeDateStyle(challengeOpenData.challengePeriod.start));
+  form.append('challengeEndDT', changeDateStyle(challengeOpenData.challengePeriod.end));
+  form.append('limitNum', `${challengeOpenData.peopleNum}`);
+  challengeOpenData.img && form.append('img', challengeOpenData.img);
+
   try {
     const data = await serverAxios.post('/admin/concert', {
       headers: {
