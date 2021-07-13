774372e569894a8cc38080f47f06c31eca4fa404
YeongWoooo <rdd9223@gmail.com>
2021-07-13 18:49:01 +0900

    Feat: 마이페이지 page 퍼블리싱

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -11,14 +11,33 @@ import { MyCommentList, MyPageHeader } from 'components/organisms';
 import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userState } from 'stores/user';
+import Styled from 'styled-components';
+import { palette } from 'styled-tools';
+import {
+  ILearnMySelf,
+  IMyPageHeader,
+  IMyScrappedLearnMyself,
+  IMyScrappedShareTogether,
+  IMyUserCommentResponse,
+  IMyUserLearnMyself,
+} from 'types/myPage';
+import { IShareTogether } from 'types/shareTogether';
+import { changeDateFormat } from 'utils';
+
 
 function MyPage(): React.ReactElement {
+  const [selectedSection, setSelectedSection] = useState('scrap');
   const [userInfo, setUserInfo] = useState<IMyPageHeader | null>(null);
   const [shareTogetherData, setShareTogetherData] = useState<IMyScrappedShareTogether | null>(null);
   const [learnMyselfData, setLearnMyselfData] = useState<IMyScrappedLearnMyself | null>(null);
   const [userLearnMyselfData, setUserLearnMyselfData] = useState<IMyUserLearnMyself | null>(null);
   const [userCommentData, setUserCommentData] = useState<IMyUserCommentResponse | null>(null);
   const globalUserInfo = useRecoilValue(userState);
+
+  const onChangeSection = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedSection(e.target.id);
+  };
+
   const fetchMyPageUserInfo = async () => {
     const data = await getMyPageUserInfo(TOKEN);
     setUserInfo(data);
@@ -81,5 +100,127 @@ function MyPage(): React.ReactElement {
     ));
   };
 
+  return (
+    <Wrapper>
+      <div className="mypage__header">
+        <MyPageHeader userInfo={userInfo} />
+      </div>
+      <div className="mypage__body">
+        <Switch>
+          <li>
+            <SwitchRadio type="radio" id="scrap" onChange={onChangeSection} checked={selectedSection === 'scrap'} />
+            <label className="subhead5" htmlFor="scrap">
+              스크랩
+            </label>
+          </li>
+          <li>
+            <SwitchRadio
+              type="radio"
+              id="activity"
+              onChange={onChangeSection}
+              checked={selectedSection === 'activity'}
+            />
+            <label className="subhead5" htmlFor="activity">
+              나의 활동
+            </label>
+          </li>
+        </Switch>
+        <div className="mypage__top">
+          {selectedSection === 'scrap' ? (
+            <MyPageSection
+              title="Share Together"
+              subTitle={`${globalUserInfo.nickname}님이 스크랩한 강연들이에요`}
+              column={2}
+              gap={30}
+              path="/mypage/share-together/scrap/more"
+              data={shareTogetherData?.mypageConcertScrap}
+              renderItemList={renderScrappedShareTogether}
+            />
+          ) : (
+            <MyPageSection
+              title="작성한 글"
+              subTitle={`${globalUserInfo.nickname}님이 작성한 런마셀들이에요`}
+              column={4}
+              gap={15}
+              path="/mypage/learn-myself/scrap/more"
+              data={userLearnMyselfData?.mypageChallengeScrap}
+              renderItemList={renderLearnMyself}
+            />
+          )}
+        </div>
+        <div className="mypage__bottom">
+          {selectedSection === 'scrap' ? (
+            <MyPageSection
+              title="Learn Myself"
+              subTitle={`${globalUserInfo.nickname}님이 스크랩한 런마셀들이에요`}
+              column={4}
+              gap={15}
+              path="/mypage/learn-myself/mine/more"
+              data={learnMyselfData?.mypageChallengeScrap}
+              renderItemList={renderLearnMyself}
+            />
+          ) : (
+            userCommentData && <MyCommentList userCommentData={userCommentData} />
+          )}
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const SwitchRadio = Styled.input`
+  display: none;
+  
+  + label {
+    cursor: pointer;
+    color: ${palette('grayscale', 3)};
+  }
+  :checked + label {
+    color: ${palette('grayscale', 6)};
+    padding-bottom: 2px;
+    background: 
+      linear-gradient(to right, ${palette('primary', 3)}, ${palette('primary', 0)})
+      left 
+      bottom
+      #fff
+      no-repeat; 
+    background-size: 100% 2px;
+  }
+`;
+
+const Switch = Styled.ul`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+  li {
+    margin: 0 20px;
+  }
+`;
+
+const Wrapper = Styled.div`
+  .mypage {
+    &__top, &__bottom, &__header {
+      display: flex;
+      justify-content: center;
+      width: 100vw;
+    }
+    &__header {
+      height: 581px;
+      background-color: ${palette('grayscale', -2)};
+    }
+    &__body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    &__top {
+      margin: 120px 0 150px 0;
+    }
+    &__bottom {
+      padding: 150px 0;
+      background-color: #f7f7f7;
+    }
+  }
+`;
 
 export default MyPage;
