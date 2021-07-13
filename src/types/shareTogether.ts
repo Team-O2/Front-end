34b00a43e36da301ca1cc45c2dde7406f37aa776
YeongWoooo <rdd9223@gmail.com>
2021-07-13 18:51:35 +0900

    Feat: Mypage에 필요한 타입 선언

--- src/types/shareTogether.ts
+++ src/types/shareTogether.ts
@@ -1,1 +1,19 @@
-export {};
+export interface IShareTogether {
+  _id: string;
+  user: string; // 유저 객체
+  title: string;
+  videoLink: string;
+  imgThumbnail: string;
+  text: string;
+  likes: number;
+  commentNum: number;
+  scrapNum: number;
+  generation: number;
+  interest: string[];
+  hashtag: string[];
+  isDeleted: boolean;
+  comments: string[]; // 코멘트 객체
+  isNotice: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
