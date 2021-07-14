85b03962fb1acb173a191db226bc3aeb1a52a802
Hyoin-Kim <hi980506@naver.com>
2021-07-14 15:38:04 +0900

    feat : challenge Register구현

--- src/components/templates/LearnMyself/ChallengeRegister.tsx
+++ src/components/templates/LearnMyself/ChallengeRegister.tsx
@@ -1,9 +1,11 @@
 import Register from 'components/molecules/ChallengeRegister';
 import React from 'react';
+import Header from '../../organisms/Header';
 
 const ChallengeRegister = () => {
   return (
     <div>
+      <Header />
       <Register />
     </div>
   );
