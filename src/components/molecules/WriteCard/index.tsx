635005ca4344c771643f8dd55cc077c708d6771d
Hyoin-Kim <hi980506@naver.com>
2021-07-14 15:38:48 +0900

    feat : challenge write

--- src/components/molecules/WriteCard/index.tsx
+++ src/components/molecules/WriteCard/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import { writeForm } from 'libs/getChallenge';
+import React, { useEffect, useState } from 'react';
 import Styled from 'styled-components';
 import CharacterBlack from '../../../assets/images/character_black.svg';
 import CharacterColor1 from '../../../assets/images/character_color1.svg';
 import CharacterColor2 from '../../../assets/images/character_color2.svg';
 import CharacterColor3 from '../../../assets/images/character_color3.svg';
-import GroupIcon from '../../../assets/images/groupIcon.svg';
-import ChallengeTag from '../../molecules/ChallengeWriteTag/index';
+import MoreIcon from '../../../assets/images/moreIcon.svg';
+import { interestList } from '../../../resources/string';
+import Button from '../../atoms/Button';
 import Modal from '../../atoms/Modal/index';
 
 export interface IProps {
@@ -14,11 +16,52 @@ export interface IProps {
   value?: string;
 }
 
-type MyFormProps = {
-  onSubmit: (form: { description1: string; description2: string; description3: string }) => void;
+type selectedStyle = {
+  backgroundColor: string;
+  color: string;
 };
 
-function WriteCard({ onSubmit }: MyFormProps) {
+type selectedStyleLists = {
+  [key: string]: selectedStyle;
+};
+
+const selectedInterestStyle: selectedStyleLists = {
+  '건강 및 피트니스': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '게임': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '교육': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '그래픽 및 디자인': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '금융': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  'K-pop': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '뉴스, 신문': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '데이트': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '날씨': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '네비게이션': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '출판,도서': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '뷰티': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '라이프 스타일': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '만화': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '부동산 / 홈 인테리어': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '사진 및 비디오': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '쇼핑': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '비즈니스': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '생산성': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '소셜 네트워킹': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '스포츠': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '어린이': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  'AR앱': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '여행': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '유틸리티': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '음식 및 음료': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '음악': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '의료': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+  '기타': { backgroundColor: '#6f6f6f', color: '#FFFFFF' },
+};
+
+type MyFormProps = {
+  onChangeForm: (form: { description1: string; description2: string; description3: string }) => void;
+};
+
+function WriteCard({ onChangeForm }: MyFormProps) {
   const maxByte = 1000; //최대 1000바이트
   const [isBadgeModal, setIsBadgeModal] = useState(false); //뱃지 모달
   const [countProgressBar, setCountProgressBar] = useState(0); //프로그래스바
@@ -45,14 +88,24 @@ function WriteCard({ onSubmit }: MyFormProps) {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: any) => {
+    setUserData({ ...userData, interest: selectedInterest });
     e.preventDefault();
-    onSubmit(form);
+    onChangeForm(form);
     setForm({
       description1: '',
       description2: '',
       description3: '',
     });
+    const writeData = {
+      good: description1,
+      bad: description2,
+      learn: description3,
+      interest: selectedInterest,
+      generation: 2,
+    };
+    const data = await writeForm(writeData);
+    console.log('getData', data);
   };
 
   const progressBarState = () => {
@@ -103,6 +156,55 @@ function WriteCard({ onSubmit }: MyFormProps) {
   useEffect(() => {
     progressBarState();
   }, [byte]);
+
+  const [isOpenTag, setIsOpenTag] = useState(false);
+  const [isClickTag, setIsClickTag] = useState(false);
+  const [isInterestModalOpen, setIsInterestModalOpen] = useState(false);
+
+  const [userData, setUserData] = useState({
+    interest: [''],
+  });
+  const [selectedInterest, setSelectedInterest] = useState<string[]>([]);
+  const modalInterestHandler = (interest: string) => {
+    if (selectedInterest.length === 0) {
+      setSelectedInterest([interest]);
+    }
+    if (selectedInterest.length < 30) {
+      if (!selectedInterest.includes(interest)) {
+        setSelectedInterest([...selectedInterest, interest]);
+      } else {
+        setSelectedInterest(selectedInterest.filter((v) => v !== interest));
+      }
+    } else {
+      if (selectedInterest.includes(interest)) {
+        setSelectedInterest(selectedInterest.filter((v) => v !== interest));
+      }
+    }
+  };
+
+  const setInterestButton = (id: any, interest: string) => {
+    return (
+      <button
+        className="tag__interest"
+        key={id}
+        onClick={() => {
+          modalInterestHandler(interest);
+          setIsClickTag(!isClickTag);
+        }}
+        style={
+          selectedInterestStyle[interest] && selectedInterest.indexOf(interest) > -1
+            ? selectedInterestStyle[interest]
+            : {}
+        }
+      >
+        {interest}
+      </button>
+    );
+  };
+
+  useEffect(() => {
+    setSelectedInterest(selectedInterest);
+  }, [selectedInterest]);
 
   return (
     <>
@@ -200,18 +302,42 @@ function WriteCard({ onSubmit }: MyFormProps) {
           ></textarea>
         </div>
 
-        <ChallengeTag />
-        <form onSubmit={handleSubmit}>
-          {countProgressBar === 3 ? (
-            <div className="button">
-              <button className="write__button-color">글 올리기</button>
-            </div>
-          ) : (
-            <div className="button">
-              <button className="write__button-black">글 올리기</button>
-            </div>
-          )}
-        </form>
+        <div className="tag">
+          해시태그
+          <div className="tag__list">
+            {interestList.map((interest, id) => {
+              if (id < 5) {
+                return setInterestButton(id, interest);
+              }
+            })}
+            <Button
+              onClick={() => {
+                setIsOpenTag(!isOpenTag);
+              }}
+            >
+              <img className="tag__moreIcon" src={MoreIcon} alt=""></img>
+            </Button>
+            {isOpenTag === true ? (
+              interestList.map((interest, id) => {
+                return setInterestButton(id, interest);
+              })
+            ) : (
+              <div className="modal__base"></div>
+            )}
+          </div>
+        </div>
+
+        {countProgressBar === 3 ? (
+          <div className="button">
+            <button className="write__button-color" onClick={handleSubmit}>
+              글 올리기
+            </button>
+          </div>
+        ) : (
+          <div className="button">
+            <button className="write__button-black">글 올리기</button>
+          </div>
+        )}
 
         <Modal isOpen={isBadgeModal} setIsOpen={setIsBadgeModal} isBlur={true}>
           <div className="badge">
@@ -243,7 +369,6 @@ const SWriteCard = Styled.div`
         margin: 0 auto;
 
         &__title{
-            font-style: normal;
             font-weight: bold;
             font-size:22px;
             line-height:38px;
@@ -251,7 +376,6 @@ const SWriteCard = Styled.div`
             color: #3D3D3D;
         }
         &__restriction{
-            font-style: normal;
             font-weight: bold;
             font-size: 14px;
             line-height: 18px;
@@ -362,8 +486,39 @@ const SWriteCard = Styled.div`
             height: 5px;
             background-color: #3abff7;
         }
-
     }
+
+    .tag{
+      width:844px;
+      align-items:center;
+      margin:0 auto;
+      padding-top:60px;
+      font-family: AppleSDGothicNeo;
+      font-size: 24px;
+      font-weight: bold;
+      line-height: 1.33;
+      letter-spacing: -0.5px;
+      text-align: left;
+      color: var(--colors-grayscale-6-f);
+
+      &__moreIcon{
+        vertical-align : middle;
+        padding-bottom:5px;
+      }
+  
+      &__interest{
+          padding : 12px 30px;
+          margin : 12px 5px;
+          height : 48px;
+          border-radius: 60px;
+          border: solid 1px #8b8b8b;
+          font-size: 18px;
+          font-weight: bold;
+          line-height: 1.33;
+          letter-spacing: -0.5px;
+          color : #8b8b8b;
+          background-color:#FFFFFF;
+        }
 
 `;
 
---
+++ src/libs/getChallenge.ts
@@ -0,0 +1,103 @@
+import axios from 'axios';
+
+export const serverAxios = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  withCredentials: true,
+});
+
+const instance = axios.create({
+  baseURL: 'http://3.37.218.69:5000',
+  withCredentials: true,
+  headers: {
+    Authorization:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjBlZDg2NTZkOWM0ZTg0NzM4NzM1OTYyIn0sImlhdCI6MTYyNjE5MjU3OCwiZXhwIjoxNjI3NDAyMTc4fQ.bR187F7yyeJJdI88EqR_imhJhbq8OJ0SueupCeVgLFQ',
+  },
+});
+
+interface WriteData {
+  good: string;
+  bad: string;
+  learn: string;
+  interest: string[];
+  generation: number;
+}
+
+interface CommentData {
+  parentID?: string;
+  text: string;
+}
+
+interface LikeData {
+  like: string;
+}
+
+interface SignData {
+  challengeCNT: number;
+}
+
+export const writeForm = async (writeData: WriteData) => {
+  console.log('data', writeData);
+  const data = await instance
+    .post('/challenge', writeData)
+    .then((response) => {
+      console.log('[SUCCESS] POST data', response);
+    })
+    .catch((error) => {
+      console.log('[FAIL] POST data', error);
+    });
+};
+
+export const ChallengeListData = async () => {
+  try {
+    const data = await instance.get('/challenge', {
+      params: {
+        offset: 1,
+        limit: 10,
+      },
+    });
+    return data.data.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const ChallengeLike = async (likeData: LikeData) => {
+  console.log('data', likeData);
+  const data = await instance
+    .post('/challenge/like/')
+    .then((response) => {
+      console.log('[SUCCESS] POST data', response);
+    })
+    .catch((error) => {
+      console.log('[FAIL] POST data', error);
+    });
+};
+
+export const ChallengeComment = async (commentData: CommentData) => {
+  console.log('data', commentData);
+  const data = await instance
+    .post('/challenge/comment/')
+    .then((response) => {
+      console.log('[SUCCESS] POST data', response);
+    })
+    .catch((error) => {
+      console.log('[FAIL] POST data', error);
+    });
+};
+
+export const SignRegister = async (signData: SignData) => {
+  console.log('data', signData);
+  const data = await instance
+    .post('user/register', signData)
+    .then((response) => {
+      console.log('[SUCCESS] POST data', response);
+    })
+    .catch((error) => {
+      console.log('[FAIL] POST data', error);
+    });
+};
+
+export const DeleteChallenge = async () => {
+  console.log('data');
+};
