b0d3fa615cc820303b86ea94a67019d13ac24b23
JiyoungHyun <shyunji023@gmail.com>
2021-07-17 03:55:01 +0900

    Etc: 이미지 클릭 시 디테일 페이지 이동

--- src/components/molecules/Concert/index.tsx
+++ src/components/molecules/Concert/index.tsx
@@ -40,7 +40,7 @@ function Concert({
           <p>{title}</p>
           <p dangerouslySetInnerHTML={{ __html: `${text}` }} />
         </div>
-        <div className="content__right">
+        <div className="content__right" onClick={onClickFunc}>
           <img className="content__right--img" src={imgThumbnail} alt="" />
           <div className="overlay overlayFade">
             <div className="content">
