823da2a4f9e8da1d51344f136a2e1411d69a184f
Subin <sbsoobin@naver.com>
2021-07-17 02:31:30 +0900

    Refactor: 로그인폼 에러메시지 추가

--- src/components/molecules/Loginform/index.tsx
+++ src/components/molecules/Loginform/index.tsx
@@ -1,5 +1,5 @@
 import Button from 'components/atoms/Button';
-import Input from 'components/atoms/Input';
+import StyledInput from 'components/atoms/StyledInput';
 import { getUserData, postLogin } from 'libs/axios';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -14,18 +14,16 @@ function Loginform(): React.ReactElement {
     email: '',
     password: '',
   });
-  const [isValueExist, setIsValueExist] = useState({
-    // 값이 들어있는지 유무
-    email: false,
-    password: false,
-  });
-  const [isFocused, setIsFocused] = useState({
-    // 인풋에 포커스가 되어있는지 유무
-    email: false,
-    password: false,
-  });
   const [userStatusData, setUserStatusData] = useRecoilState(userStatusState);
   const [userData, setUserData] = useRecoilState(userState);
+  const [isConditionMet, setIsConditionMet] = useState({
+    email: true,
+    pwd: true,
+  });
+  const errMsg = {
+    email: '가입되지 않은 이메일입니다',
+    pwd: '비밀번호를 다시 확인해 주세요',
+  };
 
   const loginBtnHandler = async () => {
     const token = await getUserStatusData();
@@ -52,7 +50,12 @@ function Loginform(): React.ReactElement {
         });
         return data.token;
       } else {
-        alert(data.message);
+        console.log(data.message);
+        if (data.message === '아이디가 존재하지 않습니다') {
+          setIsConditionMet({ email: false, pwd: true });
+        } else if (data.message === '비밀번호가 틀렸습니다') {
+          setIsConditionMet({ email: true, pwd: false });
+        }
       }
     } else {
       alert('네트워크가 좋지 않습니다');
@@ -80,58 +83,31 @@ function Loginform(): React.ReactElement {
     return false;
   };
 
-  useEffect(() => {
-    if (loginData.email != '') {
-      setIsValueExist({ ...isValueExist, email: true });
-    } else {
-      setIsValueExist({ ...isValueExist, email: false });
-    }
-  }, [loginData.email]);
-
-  useEffect(() => {
-    if (loginData.password != '') {
-      setIsValueExist({ ...isValueExist, password: true });
-    } else {
-      setIsValueExist({ ...isValueExist, password: false });
-    }
-  }, [loginData.password]);
-
   return (
-    <LoginformWrap isValueExist={isValueExist} isFocused={isFocused}>
-      <div className="login_div login_div--id">
-        <Input
-          className="login_input"
-          name="userEmail"
-          type="text"
-          placeholder="이메일을 입력해 주세요"
-          onChange={(e) => {
-            setLoginData({ ...loginData, email: e.target.value });
-          }}
-          onFocus={() => {
-            setIsFocused({ ...isFocused, email: true });
-          }}
-          onBlur={() => {
-            setIsFocused({ ...isFocused, email: false });
-          }}
-        ></Input>
-      </div>
-      <div className="login_div login_div--pwd">
-        <Input
-          className="login_input"
-          name="userPwd"
-          type="password"
-          placeholder="비밀번호를 입력해 주세요"
-          onChange={(e) => {
-            setLoginData({ ...loginData, password: e.target.value });
-          }}
-          onFocus={() => {
-            setIsFocused({ ...isFocused, password: true });
-          }}
-          onBlur={() => {
-            setIsFocused({ ...isFocused, password: false });
-          }}
-        ></Input>
-      </div>
+    <LoginformWrap>
+      <StyledInput
+        className="email"
+        placeHolder="이메일를 입력해 주세요"
+        width="406px"
+        height="60px"
+        onChange={(value) => {
+          if (typeof value === 'string') setLoginData({ ...loginData, email: value });
+        }}
+        isConditionMet={isConditionMet.email}
+        errorMsg={errMsg.email}
+      />
+      <div className="margin"></div>
+      <StyledInput
+        placeHolder="비밀번호를 입력해 주세요"
+        width="406px"
+        height="60px"
+        isPw={true}
+        onChange={(value) => {
+          if (typeof value === 'string') setLoginData({ ...loginData, password: value });
+        }}
+        isConditionMet={isConditionMet.pwd}
+        errorMsg={errMsg.pwd}
+      />
       <Button className="login_button" onClick={loginBtnHandler}>
         로그인
       </Button>
@@ -139,60 +115,14 @@ function Loginform(): React.ReactElement {
   );
 }
 
-const LoginformWrap = Styled.div<{
-  isValueExist?: { email: boolean; password: boolean };
-  isFocused: { email: boolean; password: boolean };
-}>`
+const LoginformWrap = Styled.div`
   display : flex;
   flex-direction : column;
   align-items : center;
+  .margin{
+    height : 9px;
+  }
   .login{
-    &_div{
-      width: 406px;
-      height: 60px;
-      border-radius: 4px;
-      border : solid 1px #c1c1c1;
-      margin-bottom : 16px;
-      &--id{
-        border : ${(props) =>
-          props.isValueExist?.email || props.isFocused.email ? 'double 1px transparent' : '1px solid #c1c1c1'};
-        background-image : ${(props) =>
-          props.isValueExist?.email || props.isFocused.email
-            ? 'linear-gradient(white, white), linear-gradient(to right, #36c8f5,#13e2dd)'
-            : undefined};
-        background-origin : ${(props) =>
-          props.isValueExist?.email || props.isFocused.email ? 'border-box' : undefined};
-        background-clip : ${(props) =>
-          props.isValueExist?.email || props.isFocused.email ? 'content-box, border-box' : undefined};        
-      }
-      &--pwd{
-        border : ${(props) =>
-          props.isValueExist?.password || props.isFocused.password ? 'double 1px transparent' : '1px solid #c1c1c1'};
-        background-image : ${(props) =>
-          props.isValueExist?.password || props.isFocused.password
-            ? 'linear-gradient(white, white), linear-gradient(to right, #36c8f5,#13e2dd)'
-            : undefined};
-        background-origin : ${(props) =>
-          props.isValueExist?.password || props.isFocused.password ? 'border-box' : undefined};
-        background-clip : ${(props) =>
-          props.isValueExist?.password || props.isFocused.password ? 'content-box, border-box' : undefined};        
-      }
-    }
-    &_input{
-      width : 100%;
-      height : 100%;
-      border : none;
-      border-radius: 4px;
-      padding: 18px 20px;
-      font-size : 16px;
-      color : #0d0d0d;
-      :placeholder{
-        color : #c1c1c1;
-      }
-      :focus{
-        outline : none;
-      }
-    }
     &_button{
       width: 406px;
       height: 60px;
@@ -204,7 +134,6 @@ const LoginformWrap = Styled.div<{
       margin-top : 14px;
     }
   }
-
 `;
 
 export default Loginform;
