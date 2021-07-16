f623f159af37d597c20e063b7cbed350de500e81
YeongWoooo <rdd9223@gmail.com>
2021-07-17 03:09:23 +0900

    Fix: z-index가 안먹는 버그 수정

--- src/components/atoms/Modal/index.tsx
+++ src/components/atoms/Modal/index.tsx
@@ -28,18 +28,19 @@ function Modal({ ...props }: IProps): React.ReactElement {
 }
 
 const SModal = Styled.div<{ isBlur?: boolean }>`
-.modal{
-  z-index : 3;
-  &__background{
-    position : fixed;
-    top : 0;
-    left : 0;
-    width : 100vw;
-    height : 100vh;
-    opacity : ${(props) => (props.isBlur ? 0.3 : undefined)};
-    background-color : ${(props) => (props.isBlur ? 'rgba(0,0,0,0.5)' : undefined)};
+  .modal {
+    position: absolute;
+    z-index : 100;
+    &__background {
+      position : fixed;
+      top : 0;
+      left : 0;
+      width : 100vw;
+      height : 100vh;
+      opacity : ${(props) => (props.isBlur ? 0.3 : undefined)};
+      background-color : ${(props) => (props.isBlur ? 'rgba(0,0,0,0.5)' : undefined)};
+    }
   }
-}
 `;
 
 export default Modal;
