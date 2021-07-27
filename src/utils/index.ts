8f61cd8976052e52bb02fdb60ad9c4ac73a8f59e
YeongWoooo <rdd9223@gmail.com>
2021-07-28 02:39:57 +0900

    Etc: 모듈화

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,7 @@ import dayjs from 'dayjs';
 export const changeDateFormat = (date: Date | string): string => {
   return dayjs(date).format('YYYY. MM. DD');
 };
+
+export const arrToString = (arr: string[]): string => {
+  return '[' + arr.map((item) => '"' + item + '"').toString() + ']';
+};
