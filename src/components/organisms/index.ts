d308b3851865471cdfdcc7531adf4ebffba5b9b1
JiyoungHyun <shyunji023@gmail.com>
2021-07-29 23:15:05 +0900

    Style: sharetogether import정리

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,2 +1,6 @@
+export { default as CategoryList } from './CategoryList';
+export { default as ConcertCardList } from './ConcertCardList';
+export { default as ConcertList } from './ConcertList';
 export { default as MyCommentList } from './MyCommentList';
 export { default as MyPageHeader } from './MyPageHeader';
+export { default as SearchForm } from './SearchForm';
