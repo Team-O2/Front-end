112df9485be9e1a72a7a8c26c7bd8d3c95167714
JiyoungHyun <shyunji023@gmail.com>
2021-07-30 01:23:09 +0900

    Style: ShareTogetherDetail 1단계 리팩토링

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,9 @@
 export { default as ChallengeModalComment } from './ChallengeModalComment';
 export { default as CommentedBoardRow } from './CommentedBoardRow';
+export { default as CommentWrite } from './CommentWrite';
 export { default as LearnMyselfCard } from './LearnMyselfCard';
 export { default as MyPageSection } from './MyPageSection';
 export { default as MyPageSlider } from './MyPageSlider';
+export { default as ReplyComment } from './ReplyComment';
 export { default as ShareTogetherCard } from './ShareTogetherCard';
+export { default as SingleComment } from './SingleComment';
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,6 +1,8 @@
 export { default as CategoryList } from './CategoryList';
+export { default as CommentList } from './CommentList';
 export { default as ConcertCardList } from './ConcertCardList';
 export { default as ConcertList } from './ConcertList';
+export { default as DetailContent } from './DetailContent';
 export { default as MyCommentList } from './MyCommentList';
 export { default as MyPageHeader } from './MyPageHeader';
 export { default as SearchForm } from './SearchForm';
