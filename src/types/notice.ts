39a58f5794ee6bf37d532bd174110cf1639d0fa6
JiyoungHyun <shyunji023@gmail.com>
2021-07-30 09:54:06 +0900

    Fix: notice api warning제거

---
+++ src/types/notice.ts
@@ -0,0 +1,28 @@
+export interface INoticeData {
+  videoLink: string;
+  imgThumbnail: string;
+  likes: number;
+  commentNum: number;
+  scrapNum: number;
+  interest: string[];
+  hashtag: string[];
+  isDeleted: boolean;
+  comments: string[];
+  isNotice: boolean;
+  _id: string;
+  title: string;
+  user: { _id: string; nickname: string; img: string };
+  createdAt: string;
+  text: string;
+  authorNickname: string;
+  updatedAt: string;
+  __v: number;
+}
+export interface INoticeList {
+  notices: INoticeData[];
+  totalNoticeNum: number;
+}
+export interface ISearchNoticeList {
+  searchData: INoticeData[];
+  totalNoticeSearchNum: number;
+}
