5c976023595f8573261da6a9f714be57f267f819
Subin <sbsoobin@naver.com>
2021-08-01 22:29:02 +0900

    Refactor: AdminChallengeCard 스타일 분리

--- src/components/molecules/AdminChallengeCard/index.tsx
+++ src/components/molecules/AdminChallengeCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Styled from 'styled-components';
 import { IAdminChallenge } from 'types/admin.type';
+import { BottomContainer, Exp, Image, Label, RowContainer, SAdminChallengeCard, Title } from './style';
 
 export interface IProps {
   challengeData: IAdminChallenge;
@@ -16,95 +16,42 @@ const changeDatetoString = (date: string, isIncludeYear: boolean): string => {
 function AdminChallengeCard({ challengeData }: IProps): React.ReactElement {
   return (
     <SAdminChallengeCard>
-      <img className="card__image" src={challengeData.img}></img>
-      <div className="card__container--bottom">
-        <div className="card__title">오투 회고 챌린지 {challengeData.generation}기</div>
-
-        <div className="card__container--row">
-          <div className="card__label body3">오픈일자</div>
-          <div className="card__exp subhead3">{changeDatetoString(challengeData.createdDT, true)}</div>
-        </div>
-        <div className="card__container--row">
-          <div className="card__label body3">진행기간</div>
-          <div className="card__exp subhead3">
+      <Image src={challengeData.img}></Image>
+      <BottomContainer>
+        <Title>오투 회고 챌린지 {challengeData.generation}기</Title>
+        <RowContainer>
+          <Label className=" body3">오픈일자</Label>
+          <Exp className=" subhead3">{changeDatetoString(challengeData.createdDT, true)}</Exp>
+        </RowContainer>
+        <RowContainer>
+          <Label className=" body3">진행기간</Label>
+          <Exp className=" subhead3">
             {changeDatetoString(challengeData.challengeStartDT, true)} ~{' '}
             {changeDatetoString(challengeData.challengeEndDT, false)}
-          </div>
-        </div>
-        <div className="card__container--row">
-          <div className="card__label body3">신청기간</div>
-          <div className="card__exp subhead3">
+          </Exp>
+        </RowContainer>
+        <RowContainer>
+          <Label className=" body3">신청기간</Label>
+          <Exp className=" subhead3">
             {changeDatetoString(challengeData.registerStartDT, true)} ~{' '}
             {changeDatetoString(challengeData.registerEndDT, false)}
-          </div>
-        </div>
-        <div className="card__container--row">
-          <div className="card__label body3">신청인</div>
-          <div className="card__exp subhead3">{challengeData.applyNum}명</div>
-        </div>
-        <div className="card__container--row">
-          <div className="card__label body3">참여인원수</div>
-          <div className="card__exp subhead3">{challengeData.participants}명</div>
-        </div>
-        <div className="card__container--row">
-          <div className="card__label body3">총 게시물 개수</div>
-          <div className="card__exp subhead3">{challengeData.postNum}개</div>
-        </div>
-      </div>
+          </Exp>
+        </RowContainer>
+        <RowContainer>
+          <Label className=" body3">신청인</Label>
+          <Exp className=" subhead3">{challengeData.applyNum}명</Exp>
+        </RowContainer>
+        <RowContainer>
+          <Label className=" body3">참여인원수</Label>
+          <Exp className=" subhead3">{challengeData.participants}명</Exp>
+        </RowContainer>
+        <RowContainer>
+          <Label className=" body3">총 게시물 개수</Label>
+          <Exp className=" subhead3">{challengeData.postNum}개</Exp>
+        </RowContainer>
+      </BottomContainer>
     </SAdminChallengeCard>
   );
 }
-
-const SAdminChallengeCard = Styled.div`
-    display : flex;
-    flex-direction : column;
-    align-items : center;
-    border-radius: 18px;
-    box-shadow: 0 0 24px 0 rgba(13, 12, 63, 0.1);
-    background-color: #ffffff;
-    cursor : pointer;
-    width: 402px;
-    height: 484px;
-    .card{
-        &__image{
-            margin-bottom : 30px;
-            border-top-left-radius:18px;
-            border-top-right-radius:18px;
-            width : 402px;
-            height : 148px;
-            object-fit: cover;
-        }
-        &__title{
-            margin-bottom : 40px;
-            width : 322px;
-            line-height: 1.31;
-            letter-spacing: -0.5px;
-            color : #3d3d3d;
-            font-size: 32px;
-            font-weight: bold;
-        }
-        &__container{
-            &--row{
-                display : flex;
-                flex-direction : row;
-                justify-content : flex-start;
-                margin-bottom : 9px;
-            }
-            &--bottom{
-                display : flex;
-                flex-direction : column;
-                width : 322px;
-            }
-        }
-        &__label{
-            margin-right : 41px;
-            width : 90px;
-            color:#6f6f6f;
-        }
-        &__exp{
-            color : #6f6f6f;
-        }
-    }
-  `;
 
 export default AdminChallengeCard;
---
+++ src/components/molecules/AdminChallengeCard/style.ts
@@ -0,0 +1,49 @@
+import Styled from 'styled-components';
+
+export const Label = Styled.div`
+  margin-right : 41px;
+  width : 90px;
+  color:#6f6f6f;
+`;
+export const Exp = Styled.div`
+  color : #6f6f6f;
+`;
+export const RowContainer = Styled.div`
+  display : flex;
+  flex-direction : row;
+  justify-content : flex-start;
+  margin-bottom : 9px;
+`;
+export const Image = Styled.img`
+  margin-bottom : 30px;
+  border-top-left-radius:18px;
+  border-top-right-radius:18px;
+  width : 402px;
+  height : 148px;
+  object-fit: cover;
+`;
+export const BottomContainer = Styled.div`
+  display : flex;
+  flex-direction : column;
+  width : 322px;
+`;
+export const Title = Styled.div`
+  margin-bottom : 40px;
+  width : 322px;
+  line-height: 1.31;
+  letter-spacing: -0.5px;
+  color : #3d3d3d;
+  font-size: 32px;
+  font-weight: bold;
+`;
+export const SAdminChallengeCard = Styled.div`
+  display : flex;
+  flex-direction : column;
+  align-items : center;
+  border-radius: 18px;
+  box-shadow: 0 0 24px 0 rgba(13, 12, 63, 0.1);
+  background-color: #ffffff;
+  cursor : pointer;
+  width: 402px;
+  height: 484px;
+`;
