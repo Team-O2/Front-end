a8f334b30b879ecd0fe62ce2f6dfffe9db196911
Hyoin-Kim <hi980506@naver.com>
2021-08-03 23:31:05 +0900

    Refactor : 챌린지 신청하기 뷰 css 수정

--- src/components/molecules/ChallengeDetailRegister/index.tsx
+++ src/components/molecules/ChallengeDetailRegister/index.tsx
@@ -6,7 +6,25 @@ import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { userState, userStatusState } from 'stores/user';
 import { IAdminChallengePeriod } from 'types/admin.type';
-import SRegister from './style';
+import {
+  BoxWrapper,
+  CardDetailWrapper,
+  CardSettingWrapper,
+  CardWrapper,
+  CautionWrapper,
+  ChallengeSetting,
+  ConfirmDetail,
+  ConfirmModalWrapper,
+  Container,
+  DetailContent,
+  Header,
+  ModalButton,
+  NoticeCardWrapper,
+  NoticeHeaderWrapper,
+  RegisterButton,
+  RegisterImg,
+  RegisterSubmitWrapper,
+} from './style';
 
 function ChallengeRegister(): React.ReactElement {
   const [userData, setUserData] = useRecoilState(userState);
@@ -63,136 +81,108 @@ function ChallengeRegister(): React.ReactElement {
   }, []);
 
   return periodData ? (
-    <SRegister>
+    <Container>
       <RegisterHeader title={periodData.title} generation={periodData.generation} img={periodData.img} />
-      <div className="container">
-        <div className="header__fixed">
-          <p className="header__title">챌린지 설명</p>
-          <span className="header__line"></span>
-        </div>
-        <div className="img">
-          <img className="register__img" src={ChallengeImg} alt="" />
-        </div>
-        <div className="card">
-          <div className="register">
-            <div className="register__date">신청기간</div>
-            <div className="register__remain">{getRestDay(periodData.registerEndDT)}일 남음</div>
-            <div className="challenge">챌린지 기간</div>
-            <div className="challenge__detail">
-              {dateToString(periodData.challengeStartDT)} ~ {dateToString(periodData.challengeEndDT)}
-            </div>
-            <div
-              className="challenge_setting_container"
-              style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}
-            >
-              <div className="challenge__setting">
-                <div className="challenge__count-set">챌린지 개수 설정</div>
-                <p className="challenge__notice">2개 이상부터 뱃지 획득 가능</p>
-              </div>
-              <div className="challenge__box">
-                <div
-                  className="challenge__minus-plus"
-                  onClick={() => {
-                    minusCount();
-                  }}
-                >
-                  <img src={MinusIcon} />
-                </div>
-                {registerCount}
-                <div
-                  className="challenge__minus-plus"
-                  onClick={() => {
-                    setRegisterCount(registerCount + 1);
-                  }}
-                >
-                  <img src={PlusIcon} />
-                </div>
-              </div>
-            </div>
-            <div className="button">
-              <button
-                className="button__register"
+      <Header>
+        <p>챌린지 설명</p>
+        <span></span>
+      </Header>
+      <RegisterImg>
+        <img src={ChallengeImg} alt="" />
+      </RegisterImg>
+      <CardWrapper>
+        <CardDetailWrapper>
+          <h1>신청기간</h1>
+          <h2>{getRestDay(periodData.registerEndDT)}일 남음</h2>
+          <h3>챌린지 기간</h3>
+          <h4>
+            {dateToString(periodData.challengeStartDT)} ~ {dateToString(periodData.challengeEndDT)}
+          </h4>
+          <CardSettingWrapper>
+            <ChallengeSetting>
+              <h1>챌린지 개수 설정</h1>
+              <p>2개 이상부터 뱃지 획득 가능</p>
+            </ChallengeSetting>
+            <BoxWrapper>
+              <p
                 onClick={() => {
-                  setIsOpenModal(true);
+                  minusCount();
                 }}
               >
-                신청하기
-              </button>
-              <Modal isOpen={isOpenModal} setIsOpen={setIsOpenModal} isBlur={true}>
-                <div className="modal__main">
-                  <div className="modal__main-card">
-                    <h3 className="modal__notice">잠깐! 확인하고 신청 완료하세요</h3>
-                    <p className="modal__notice-detail">아래 정보를 확인해주세요</p>
-                    <div className="modal__detail">
-                      <div>
-                        <div className="modal__title">챌린지 기간</div>
-                        <div className="modal__content">
-                          {dateToString(periodData.challengeStartDT)}~{dateToString(periodData.challengeEndDT)}
-                        </div>
-                      </div>
-                      <div>
-                        <div className="modal__title">신청자</div>
-                        <div className="modal__content">{userData?.nickname}님</div>
-                      </div>
-                      <div>
-                        <div className="modal__title">챌린지 개수</div>
-                        <div className="modal__content">{registerCount}개</div>
-                      </div>
-                    </div>
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        marginTop: '50px',
-                      }}
-                    >
-                      <img className="modal__icon" src={CautionIcon} alt=""></img>
-                      <div className="modal__caution">주의사항</div>
-                    </div>
-                    <div className="modal__caution-detail">
-                      챌린지 개수 2개를 선택하시면 최종 달성률 80%를 넘더라도 배지를 받을 수 없어요!
-                    </div>
-                  </div>
-                  <button className="modal__button" onClick={handleSubmit}>
-                    <img className="modal__img" src={CheckIcon} alt=""></img>
-                    <div className="modal__register-button">주의 사항을 확인했으며 챌린지 신청을 완료합니다.</div>
-                  </button>
-                </div>
-              </Modal>
-              {isRegisterSubmit === true ? (
-                <Modal isOpen={isRegisterSubmit} setIsOpen={setIsRegisterSubmit} isBlur={true}>
-                  <div className="submit">
-                    <img className="submit__icon" src={CheckCircleIcon} alt=""></img>
-                    <div className="submit__title">신청완료!</div>
-                    <div className="submit__detail">{userData?.nickname}님 챌린지가 정상적으로</div>
-                    <div className="submit__detail"> 신청되었습니다.</div>
-                  </div>
-                </Modal>
-              ) : null}
-            </div>
-          </div>
+                <img src={MinusIcon} />
+              </p>
+              {registerCount}
+              <p
+                onClick={() => {
+                  setRegisterCount(registerCount + 1);
+                }}
+              >
+                <img src={PlusIcon} />
+              </p>
+            </BoxWrapper>
+          </CardSettingWrapper>
+          <RegisterButton>
+            <button
+              onClick={() => {
+                setIsOpenModal(true);
+              }}
+            >
+              신청하기
+            </button>
+          </RegisterButton>
+        </CardDetailWrapper>
+        <Modal isOpen={isOpenModal} setIsOpen={setIsOpenModal} isBlur={true}>
+          <ConfirmModalWrapper>
+            <ConfirmDetail>
+              <h1>잠깐! 확인하고 신청 완료하세요</h1>
+              <h2>아래 정보를 확인해주세요</h2>
+              <DetailContent>
+                <h3>챌린지 기간</h3>
+                <p>
+                  {dateToString(periodData.challengeStartDT)}~{dateToString(periodData.challengeEndDT)}
+                </p>
+                <h3>신청자</h3>
+                <p>{userData?.nickname}님</p>
+                <h3>챌린지 개수</h3>
+                <p>{registerCount}개</p>
+              </DetailContent>
+              <CautionWrapper>
+                <img src={CautionIcon} alt=""></img>
+                <h4>주의사항</h4>
+              </CautionWrapper>
+              <h5>챌린지 개수 2개를 선택하시면 최종 달성률 80%를 넘더라도 배지를 받을 수 없어요!</h5>
+            </ConfirmDetail>
+            <ModalButton onClick={handleSubmit}>
+              <img src={CheckIcon} alt=""></img>
+              <p>주의 사항을 확인했으며 챌린지 신청을 완료합니다.</p>
+            </ModalButton>
+          </ConfirmModalWrapper>
+        </Modal>
+        {isRegisterSubmit === true ? (
+          <Modal isOpen={isRegisterSubmit} setIsOpen={setIsRegisterSubmit} isBlur={true}>
+            <RegisterSubmitWrapper>
+              <img src={CheckCircleIcon} alt=""></img>
+              <h5>신청완료!</h5>
+              <p>{userData?.nickname}님 챌린지가 정상적으로</p>
+              <p> 신청되었습니다.</p>
+            </RegisterSubmitWrapper>
+          </Modal>
+        ) : null}
 
-          <div className="card__notice">
-            <div className="o2__notice">
-              오투 챌린지 주의사항
-              <img className="notice__img" src={AlertIcon}></img>
-            </div>
-            <div className="notice__num">01</div>
-            <div className="notice__detail">
-              주당 회고록 개수 3개 이상 선택 및 80% 이상 달성 시 뱃지 획득이 가능합니다.
-            </div>
-            <div className="notice__num">02</div>
-            <div className="notice__detail">
-              본 챌린지 의도와는 다른 광고성 글과 카테고리와 무관한 내용은 무통보 삭제 될 수 있습니다.
-            </div>
-          </div>
-        </div>
-      </div>
-    </SRegister>
+        <NoticeCardWrapper>
+          <NoticeHeaderWrapper>
+            오투 챌린지 주의사항
+            <img src={AlertIcon}></img>
+          </NoticeHeaderWrapper>
+          <h1>01</h1>
+          <p>주당 회고록 개수 3개 이상 선택 및 80% 이상 달성 시 뱃지 획득이 가능합니다.</p>
+          <h1>02</h1>
+          <p>본 챌린지 의도와는 다른 광고성 글과 카테고리와 무관한 내용은 무통보 삭제 될 수 있습니다.</p>
+        </NoticeCardWrapper>
+      </CardWrapper>
+    </Container>
   ) : (
-    <div> 챌린지 신청기간이 아닙니다</div>
+    <p> 챌린지 신청기간이 아닙니다</p>
   );
 }
 export default ChallengeRegister;
