cbddf12711fc971bc1449d4dc05b1a9c795ed2cd
Hyoin-Kim <hi980506@naver.com>
2021-08-03 23:32:35 +0900

    Style : 챌린지 댓글 css 수정

--- src/components/molecules/ChallengeSingleComment/style.ts
+++ src/components/molecules/ChallengeSingleComment/style.ts
@@ -1,54 +1,58 @@
 import Styled from 'styled-components';
+import { palette, theme } from 'styled-tools';
 
-const SSingleComment = Styled.div`
+export const CommentWrapper = Styled.div`
+  display: flex;
+  align-items:flex-start;
+  justify-content: space-between;
+  margin: 0 auto ;
+  width: 724px;  
+
+  img{
+    border-radius : 100%;
+    width: 28px;
+    height: 28px;
+  }
+  h1{
+    margin: 0 13px;
+    width : 86px;
+    ${theme('font.subhead3')};
+    ${palette('grayscale', 9)};
+  }
+  h2{
+    margin-right : 8px;
+    width : 585px;
+    white-space : normal;
+    word-break:break-all;
+    color: ${palette('grayscale', 5)};
+    ${theme('font.body3')};
+
+  }
+`;
+
+export const CommentFold = Styled.div`
+  cursor : pointer;
+  width: 50px;
+  text-align : right;
+  white-space : nowrap;
+  color: ${palette('primary', 3)};
+  ${theme('font.subhead2')};
+`;
+
+export const RecommentWrapper = Styled.div`
   display: flex;
   flex-direction: column;
-  .comment {
-    display: flex;
-    align-items:flex-start;
-    justify-content: space-between;
-    margin: 0 auto ;
-    width: 724px;
-    line-height: 1.43;
-    font-family: 'AppleSDGothicNeo';   
-    &__profile {
-      border-radius : 100%;
-      width: 28px;
-      height: 28px;
-    }
-    &__writer {
-      margin: 0 13px;
-      width : 86px;
-      font-size: 16px;
-      font-weight: bold;
-    }
-    &__text {
-      margin-right : 8px;
-      width : 585px;
-      white-space : normal;
-      word-break:break-all;
-      color: #6f6f6f;
-      font-size: 16px;
-    }
-    &__toggle {
-      cursor : pointer;
-      width: 50px;
-      text-align : right;
-      white-space : nowrap;
-      color: #36c8f5;
-      font-size: 14px;
-      font-weight: bold;
-    }
-  }
+  align-items : flex-end;
+  margin: 8px 0 31px;
+`;
+
+export const SingleCommentWrapper = Styled.div`
+  display: flex;
+  flex-direction: column;
+
   .reply {
-    display: flex;
-    flex-direction: column;
-    align-items : flex-end;
-    margin: 8px 0 31px;
     &__write {
       margin-bottom:20px;
     }
   }
 `;
-
-export default SSingleComment;
