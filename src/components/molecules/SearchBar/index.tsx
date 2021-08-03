5cf0c2e7624be34f999758e5ec689ac53d98469e
JiyoungHyun <shyunji023@gmail.com>
2021-08-03 10:39:11 +0900

    Refactor: SearchForm 복구

--- src/components/molecules/SearchBar/index.tsx
+++ src/components/molecules/SearchBar/index.tsx
@@ -1,7 +1,6 @@
 import { SearchIcon } from 'assets/images';
-import { Button, Input } from 'components/atoms';
 import React from 'react';
-import Styled from 'styled-components';
+import { SButton, SearchBarWrapper, SInput } from './style';
 
 export interface IProps {
   className?: string;
@@ -11,40 +10,13 @@ export interface IProps {
 
 function SearchBar({ onClickSearch, onChangeKeyword }: IProps): React.ReactElement {
   return (
-    <SSearchBar>
-      <Input className="input" name="searchBar" onChange={onChangeKeyword} placeholder="키워드를 검색해 주세요"></Input>
-      <Button className="button" onClick={onClickSearch}>
+    <SearchBarWrapper>
+      <SInput name="searchBar" onChange={onChangeKeyword} placeholder="키워드를 검색해 주세요"></SInput>
+      <SButton onClick={onClickSearch}>
         <img src={SearchIcon} alt="" />
-      </Button>
-    </SSearchBar>
+      </SButton>
+    </SearchBarWrapper>
   );
 }
-
-const SSearchBar = Styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-right: 33px;
-  .input {
-    border: 1px solid #dfdfdf;
-    border-radius: 72px;
-    background-color: rgba(223, 223, 223, 0.2);
-    padding: 9px 18px 9px 20px;
-    width: 298px;
-    height: 46px;
-    color: #c1c1c1;
-    font-size: 18px;
-    :focus {outline:none;}
-  }
-  .button {
-    opacity: 50%;
-    margin-bottom:4px;
-    margin-left:-50px;
-    border: #ffffff;
-    background-color: rgba(223, 223, 223, 0.2);
-    width: 24px;
-    height: 24px;
-  }
-`;
 
 export default SearchBar;
--- src/components/organisms/SearchForm/index.tsx
+++ src/components/organisms/SearchForm/index.tsx
@@ -1,7 +1,7 @@
 import { DiamondIcon } from 'assets/images';
 import { SearchBar } from 'components/molecules';
 import React, { useState } from 'react';
-import Styled from 'styled-components';
+import { Detail, Main, SearchFormWrapper } from './style';
 
 interface IProps {
   reRenderKeyword?: (keyword: string) => void;
@@ -21,51 +21,17 @@ function SearchForm({ reRenderKeyword, selectedCategory, contentListNum, keyword
     keywordChange && keywordChange();
   };
   return (
-    <SSearchForm>
-      <div className="main">
-        <img className="main__img" src={DiamondIcon} alt="" />
-        <p className="main__title">{selectedCategory}</p>
-        <SearchBar
-          onChangeKeyword={onChangeKeyword}
-          onClickSearch={onClickSearch}
-          className="main__searchbar"
-        ></SearchBar>
-      </div>
-      <div className="detail">
+    <SearchFormWrapper>
+      <Main>
+        <img src={DiamondIcon} alt="" />
+        <p>{selectedCategory}</p>
+        <SearchBar onChangeKeyword={onChangeKeyword} onClickSearch={onClickSearch}></SearchBar>
+      </Main>
+      <Detail>
         <p>{contentListNum}개의 콘텐츠</p>
-      </div>
-    </SSearchForm>
+      </Detail>
+    </SearchFormWrapper>
   );
 }
-
-const SSearchForm = Styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 80px;
-  .main {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    &__img {
-        width: 22px;
-        height: 22px;
-    }
-    &__title {
-        flex:1;
-        margin-left: 10px;
-        color: #242424;
-        font-size: 32px;
-        font-weight: bold;
-    }
-    &__searchbar {
-        margin-right:33px;
-    }
-  }
-  .detail {
-    margin-top:12px;
-    color: #3d3d3d;
-    font-size: 16px;
-  }
-`;
 
 export default SearchForm;
