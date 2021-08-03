2b7e4bf0b93eed6fbb64f1999de379494714cd18
Subin <sbsoobin@naver.com>
2021-08-04 00:14:20 +0900

    Refactor: Login style 분리

--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -1,71 +1,25 @@
 import { LineIcon } from 'assets/images';
-import { Button, Label, Link } from 'components/atoms';
+import { Link } from 'components/atoms';
 import { LoginForm } from 'components/molecules';
 import React from 'react';
-import Styled from 'styled-components';
+import { ButtonWrapper, FindPwdBtn, JoinBtn, Line, LoginContainer, LoginLabel } from './style';
 
 function CLogin(): React.ReactElement {
   return (
-    <LoginWrap>
-      <Label className="login__label">로그인</Label>
+    <LoginContainer>
+      <LoginLabel className="login__label">로그인</LoginLabel>
       <LoginForm />
-      <div className="etc__buttons">
+      <ButtonWrapper>
         <Link to="/setting/password/find">
-          <Button className="etc__leftbtn">비밀번호 찾기</Button>
+          <FindPwdBtn>비밀번호 찾기</FindPwdBtn>
         </Link>
-        <img className="etc__line" src={LineIcon}></img>
+        <Line src={LineIcon}></Line>
         <Link to="/join">
-          <Button className="etc__rightbtn">회원가입</Button>
+          <JoinBtn>회원가입</JoinBtn>
         </Link>
-      </div>
-    </LoginWrap>
+      </ButtonWrapper>
+    </LoginContainer>
   );
 }
-
-const LoginWrap = Styled.div`
-  display : flex;
-  position: relative;
-  top: -60px;
-  flex-direction : column;
-  align-items : center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh;
-  .login{
-    &__label{
-      margin-bottom : 60px;
-      font-size: 40px;
-      font-weight: bold;
-    }
-  }
-  .etc{
-    &__buttons{
-      display : flex;
-      align-items: center;
-      margin-top : 20px;
-    }
-    &__line{
-      background-color : #8b8b8b;
-      width : 1px;
-      height : 16px;
-      object-fit : contain;
-    }
-    &__leftbtn{
-      margin-right : 15px;
-      line-height : 1.5;
-      color : #8b8b8b;
-      font-family: AppleSDGothicNeo;
-      font-size : 16px;
-    }
-    &__rightbtn{
-      margin-left : 15px;
-      line-height : 1.5;
-      color : #8b8b8b;
-      font-family: AppleSDGothicNeo;
-      font-size : 16px;
-      font-weight : bold;
-    }
-  }
-`;
 
 export default CLogin;
