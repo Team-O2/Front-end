c1f2e43b008318ae6915409de9ad52df21dc399f
JiyoungHyun <shyunji023@gmail.com>
2021-08-03 10:26:08 +0900

    Refactor: NoticeHeader 복구

--- src/components/organisms/NoticeHeader/index.tsx
+++ src/components/organisms/NoticeHeader/index.tsx
@@ -1,58 +1,22 @@
-import { NoticeBackgroundIcon } from 'assets/images';
-import { Button, Link } from 'components/atoms';
+import { NoticeHeaderIcon } from 'assets/images';
+import { Link } from 'components/atoms';
 import React from 'react';
-import Styled from 'styled-components';
+import { Background, NoticeHeaderWrapper, SButton, Text } from './style';
 
 function NoticeHeader(): React.ReactElement {
   return (
-    <SNoticeHeader>
-      <img className="background" src={NoticeBackgroundIcon} alt="" />
-      <div className="text">
+    <NoticeHeaderWrapper>
+      <Background />
+      <img src={NoticeHeaderIcon} alt="" />
+      <Text>
         <p>다음 강연이에요</p>
         <p>창업에 대한 모든 것</p>
         <Link to={`/concert`}>
-          <Button className="button">더 알아보기</Button>
+          <SButton>더 알아보기</SButton>
         </Link>
-      </div>
-    </SNoticeHeader>
+      </Text>
+    </NoticeHeaderWrapper>
   );
 }
-const SNoticeHeader = Styled.div`
-  position: relative;
-  margin-bottom: 50px;
-  .background {
-    width: 100%;
-  }
-  .text {
-    display: flex;
-    position:absolute;
-    top: 50%;
-	left: 30%;
-    flex-direction: column;
-	transform: translate( -50%, -50% );
-    color: white;
-    & p:nth-of-type(1) {
-      line-height: 1.42;
-      font-size: 48px;
-      font-weight: bold;
-    }
-    & p:nth-of-type(2) {
-      line-height: 1.33;
-      font-size: 18px;
-      font-weight: bold;
-    }
-  .button {
-    margin-top:37px;
-    margin-bottom:85px;
-    border-radius: 4px;
-    background-color: white;
-    width: 175px;
-    height: 44px;
-    color: #13e2dd;
-    font-family: 'AppleSDGothicNeo';
-    font-size: 18px;
-    font-weight: bold;
-  }
-}
-`;
+
 export default NoticeHeader;
