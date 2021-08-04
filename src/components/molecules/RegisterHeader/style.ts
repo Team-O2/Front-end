1e9d60e6dd0b91e41f8ed4b7a6564a400671dd79
Hyoin-Kim <hi980506@naver.com>
2021-08-04 14:18:16 +0900

    Style : p태그 제거

--- src/components/molecules/RegisterHeader/style.ts
+++ src/components/molecules/RegisterHeader/style.ts
@@ -6,24 +6,21 @@ interface ISHeader {
 }
 
 export const GenerationText = Styled.div`
-
-  color: ${palette('grayscale', 0)};                                                                   
-  width: 600px;
-  height: 68px;
-  text-align: left;
-  ${theme('font.dp3_eng')};
+    color: ${palette('grayscale', 0)};                                                                   
+    width: 600px;
+    height: 68px;
+    text-align: left;
+    ${theme('font.dp3_eng')};
 
 `;
 
 export const Container = Styled.div<ISHeader>`
-    p{
-        width: 100%;
-        height:253px;
-        position:relative;
-        background: url(${(props) => props.url}) center center / cover;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+    width: 100%;
+    height:253px;
+    position:relative;
+    background: url(${(props) => props.url}) center center / cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
 `;
