d414f990e9f83b98aadaeddeb9de9715f2acbfa8
Subin <sbsoobin@naver.com>
2021-08-06 10:37:48 +0900

    Refactor: 랜딩페이지 style 분리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,12 +20,50 @@ import {
   UsefulLearnMyselfFillIcon,
   WelcomeToO2FillIcon,
 } from 'assets/images';
-import { Button } from 'components/atoms';
 import { BadgeCard } from 'components/molecules';
 import React, { useEffect, useState } from 'react';
 import Reveal from 'react-awesome-reveal';
 import { Link } from 'react-router-dom';
 import Styled from 'styled-components';
+import {
+  BadgeWrapper,
+  ChallengeBtn,
+  ChallengeCard,
+  ChallengeCardWrapper,
+  ChallengeWrapper,
+  ConcertContent,
+  ConcertExpContainer1,
+  ConcertExpContainer2,
+  ConcertExpType,
+  ConcertImage1,
+  ConcertImage2,
+  ConcertTitle,
+  ConcertWrapper,
+  ConcertWrapper1,
+  ConcertWrapper2,
+  LinkBtn,
+  LinkCardWrapper,
+  LinkExp,
+  LinkTitle,
+  LinkWrapper,
+  MiddleExp,
+  MiddleSubTitle,
+  MiddleTitle,
+  O2BadgeSubTitle,
+  O2BadgeTitle,
+  O2BadgeWrapper,
+  O2SpaekerExp,
+  O2SpeakerBadgeImage,
+  O2SpeakerInnerWrapper,
+  O2SpeakerOuterWrapper,
+  O2SpeakerSubTitle,
+  O2SpeakerTitle,
+  O2SpeakerWrapper,
+  O2Title,
+  TopExp,
+  TopSubTitle,
+  TopWrapper,
+} from './style';
 
 const moveUp = keyframes`
   from {
@@ -50,78 +88,78 @@ function Home(): React.ReactElement {
   }, []);
 
   return (
-    <JoinWrap>
+    <HomeContainer>
       <Reveal keyframes={moveUp} cascade>
-        <div className="top">
-          <h1 className="top__title--O2 ">O2</h1>
-          <h2 className="dp2 top__title--subtitle">
+        <TopWrapper>
+          <O2Title>O2</O2Title>
+          <TopSubTitle>
             함께 성장하는 공간, <br />
             창업가들이 마시는 산소
-          </h2>
-          <p className="top__exp body4">답답한 창업 여정에 시원한 산소 한 모금</p>
+          </TopSubTitle>
+          <TopExp>답답한 창업 여정에 시원한 산소 한 모금</TopExp>
           <Link to={`/challenge/${generationNum}`}>
-            <Button className="top__button subhead2">챌린지 보러가기</Button>
+            <ChallengeBtn>챌린지 보러가기</ChallengeBtn>
           </Link>
-        </div>
+        </TopWrapper>
       </Reveal>
-      <div className="learnMyself">
+      <ChallengeWrapper>
         <Reveal keyframes={moveUp}>
-          <h2 className="middle__title--title dp1_eng">Learn Myself</h2>
-          <h3 className="middle__title--subtitle h1">나에게 배우다, 나를 배우다.</h3>
-          <p className="middle__exp body4">
+          <MiddleTitle>Learn Myself</MiddleTitle>
+          <MiddleSubTitle>나에게 배우다, 나를 배우다.</MiddleSubTitle>
+          <MiddleExp>
             오늘의 잘한 점과 못한 점, 배운 점을 기록해요.
             <br />
             배운 점을 적용하며 더 성장한 내일의 나를 만들어요.
-          </p>
+          </MiddleExp>
         </Reveal>
         <Reveal cascade damping={0.3} keyframes={moveUp} triggerOnce>
-          <ul className="learnMyself__container--card">
-            <img className="learnMyself__img--card" src={MainCard1Img} />
-            <img className="learnMyself__img--card" src={MainCard2Img} />
-            <img className="learnMyself__img--card" src={MainCard3Img} />
-            <img className="learnMyself__img--card" src={MainCard4Img} />
-          </ul>
+          <ChallengeCardWrapper>
+            <ChallengeCard src={MainCard1Img} />
+            <ChallengeCard src={MainCard2Img} />
+            <ChallengeCard src={MainCard3Img} />
+            <ChallengeCard src={MainCard4Img} />
+          </ChallengeCardWrapper>
         </Reveal>
-      </div>
-      <div className="shareTogether">
+      </ChallengeWrapper>
+      <ConcertWrapper>
         <Reveal keyframes={moveUp}>
-          <h2 className="middle__title--title dp1_eng">Share Together</h2>
-          <h3 className="middle__title--subtitle h1">경험을 나누다, 함께 성장하다.</h3>
-          <p className="middle__exp body4">
+          <MiddleTitle>Share Together</MiddleTitle>
+          <MiddleSubTitle>경험을 나누다, 함께 성장하다.</MiddleSubTitle>
+          <MiddleExp>
             누구나 O2 Speaker가 될 수 있어요.
             <br />
             O2 People 앞에서 당신의 이야기를 나눠주세요.
-          </p>
+          </MiddleExp>
         </Reveal>
-        <div className="shareTogether__container--photo1">
-          <img className="shareTogether__image1" src={Concert1Img}></img>
-          <div className="shareTogether__container--exp1">
+        <ConcertWrapper1>
+          <ConcertImage1 src={Concert1Img}></ConcertImage1>
+          <ConcertExpContainer1>
             <Reveal keyframes={moveUp}>
-              <p className="body3 shareTogether__exp--type">Share Together 인기글</p>
-              <h1 className="h1 shareTogether__title">
+              <ConcertExpType>Share Together 인기글</ConcertExpType>
+              <ConcertTitle>
                 내가 힘들 때 버틸 수 있었던 <br />
                 3가지
-              </h1>
-              <p className="shareTogether__content">
+              </ConcertTitle>
+              <ConcertContent>
                 창업을 하다보면 정말 생각하지 못했던 어려운 일들이 <br />
                 많이 일어나는건 누구나 알고계실거라 생각합니다. <br />
                 중간에 포기하고 싶어지는 순간에 앵엘님의
                 <br />
                 [내가 힘들 때 버틸 수 있었던 3가지] 강연으로 나만의 <br />
                 버팀목도 한번 만들어보세요.
-              </p>
+              </ConcertContent>
             </Reveal>
-          </div>
-        </div>
-        <div className="shareTogether__container--photo2">
-          <div className="shareTogether__container--exp2">
+          </ConcertExpContainer1>
+        </ConcertWrapper1>
+        <ConcertWrapper2>
+          <ConcertExpContainer2>
             <Reveal keyframes={moveUp}>
-              <p className="body3 shareTogether__exp--type">Share Together 인기글</p>
-              <h1 className="h1 shareTogether__title">
+              <ConcertExpType>Share Together 인기글</ConcertExpType>
+              <ConcertTitle>
                 CEO라는 꿈. <br />
                 정말 나를 위한 유일한 길일까?
-              </h1>
-              <p className="shareTogether__content">
+              </ConcertTitle>
+              <ConcertContent>
                 여러분은 자신의 꿈에 얼마나 큰 확신을 가지고있나요? <br />
                 아마 꿈을 향해 나아가면서도 끊임없이 ‘내가 잘 하고 있는 것인가?‘, <br />
                 ‘이게 정말 나를 위한 길인가?’ 이런 생각이 드실거라 생각합니다. <br />
@@ -129,50 +167,50 @@ function Home(): React.ReactElement {
                 <br />
                 하기만 CEO라는 꿈을 어떻게 이룰 수 있었는지, <br />
                 깡토님의 실제 스토리를 여러분들께 들려드립니다.
-              </p>
+              </ConcertContent>
             </Reveal>
-          </div>
-          <img className="shareTogether__image2" src={Concert2Img}></img>
-        </div>
-      </div>
-      <div className="o2speaker">
+          </ConcertExpContainer2>
+          <ConcertImage2 src={Concert2Img}></ConcertImage2>
+        </ConcertWrapper2>
+      </ConcertWrapper>
+      <O2SpeakerWrapper>
         <Reveal keyframes={moveUp}>
-          <h1 className="dp1_eng o2speaker__title">To Be an O2 Speaker</h1>
-          <h2 className="subhead4 o2speaker__subtitle">뱃지를 모아 O2 Speaker가 되어보세요!</h2>
+          <O2SpeakerTitle>To Be an O2 Speaker</O2SpeakerTitle>
+          <O2SpeakerSubTitle>뱃지를 모아 O2 Speaker가 되어보세요!</O2SpeakerSubTitle>
         </Reveal>
         <Reveal cascade damping={0.5} keyframes={moveUp} triggerOnce>
-          <ul className="o2speaker__container--outer">
-            <li className="o2speaker__container--inner">
-              <img className="o2speaker__image--badge" src={SpeakerBadge1Img} />
-              <p className="body4 o2speaker__exp">
+          <O2SpeakerOuterWrapper>
+            <O2SpeakerInnerWrapper>
+              <O2SpeakerBadgeImage src={SpeakerBadge1Img} />
+              <O2SpaekerExp>
                 챌린지 개수를 주당 <br />
                 3개 이상으로 설정
-              </p>
-            </li>
-            <li className="o2speaker__container--inner">
-              <img className="o2speaker__image--badge" src={SpeakerBadge2Img} />
-              <p className="body4 o2speaker__exp">
+              </O2SpaekerExp>
+            </O2SpeakerInnerWrapper>
+            <O2SpeakerInnerWrapper>
+              <O2SpeakerBadgeImage src={SpeakerBadge2Img} />
+              <O2SpaekerExp>
                 런마쎌을 80%이상 작성 후 <br />
                 챌린지뱃지를 획득
-              </p>
-            </li>
-            <li className="o2speaker__container--inner">
-              <img className="o2speaker__image--badge" src={SpeakerBadge3Img} />
-              <p className="body4 o2speaker__exp">
+              </O2SpaekerExp>
+            </O2SpeakerInnerWrapper>
+            <O2SpeakerInnerWrapper>
+              <O2SpeakerBadgeImage src={SpeakerBadge3Img} />
+              <O2SpaekerExp>
                 챌린지뱃지 3개 + 활동뱃지 12개 <br />
                 얻을 시 연사가능
-              </p>
-            </li>
-          </ul>
+              </O2SpaekerExp>
+            </O2SpeakerInnerWrapper>
+          </O2SpeakerOuterWrapper>
         </Reveal>
-      </div>
-      <div className="o2badge">
+      </O2SpeakerWrapper>
+      <O2BadgeWrapper>
         <Reveal keyframes={moveUp}>
-          <h1 className="o2badge__title dp1_eng">O2 Badge</h1>
-          <h2 className="o2badge__subtitle subhead4">열심히 활동하면 멋진 오투의 뱃지들을 받을 수 있어요.</h2>
+          <O2BadgeTitle>O2 Badge</O2BadgeTitle>
+          <O2BadgeSubTitle>열심히 활동하면 멋진 오투의 뱃지들을 받을 수 있어요.</O2BadgeSubTitle>
         </Reveal>
         <Reveal cascade damping={0.2} keyframes={moveUp} triggerOnce>
-          <ul className="o2badge__container--badge">
+          <BadgeWrapper>
             <BadgeCard
               img={WelcomeToO2FillIcon}
               title="웰컴 투 오투"
@@ -254,57 +292,57 @@ function Home(): React.ReactElement {
                 </>
               }
             />
-          </ul>
+          </BadgeWrapper>
         </Reveal>
-      </div>
-      <div className="linkTo">
-        <div className="linkTo__container--card">
-          <h1 className="h1_eng linkTo__title">
+      </O2BadgeWrapper>
+      <LinkWrapper>
+        <LinkCardWrapper>
+          <LinkTitle>
             Learn Myself <br />
             Register
-          </h1>
-          <p className="body4 linkTo__exp">
+          </LinkTitle>
+          <LinkExp>
             Learn Myself에 <br />
             참여해 보세요!
-          </p>
+          </LinkExp>
           <Link to="/challenge/register">
             <Reveal keyframes={moveUp}>
-              <Button className="subhead4_eng linkTo__button">More</Button>
+              <LinkBtn>More</LinkBtn>
             </Reveal>
           </Link>
-        </div>
-        <div className="linkTo__container--card">
-          <h1 className="h1_eng linkTo__title">Learn Myself</h1>
-          <p className="body4 linkTo__exp">
+        </LinkCardWrapper>
+        <LinkCardWrapper>
+          <LinkTitle>Learn Myself</LinkTitle>
+          <LinkExp>
             나만의
             <br />
             챌린지를 시작해 보세요
-          </p>
+          </LinkExp>
           <Reveal keyframes={moveUp}>
             <Link to={`/challenge/${generationNum}`}>
-              <Button className="subhead4_eng linkTo__button">More</Button>
+              <LinkBtn>More</LinkBtn>
             </Link>
           </Reveal>
-        </div>
-        <div className="linkTo__container--card">
-          <h1 className="h1_eng linkTo__title">Share Together</h1>
-          <p className="body4 linkTo__exp">
+        </LinkCardWrapper>
+        <LinkCardWrapper>
+          <LinkTitle>Share Together</LinkTitle>
+          <LinkExp>
             O2 People의 이야기를
             <br />
             들어보세요!
-          </p>
+          </LinkExp>
           <Reveal keyframes={moveUp}>
             <Link to="/concert">
-              <Button className="subhead4_eng linkTo__button">More</Button>
+              <LinkBtn>More</LinkBtn>
             </Link>
           </Reveal>
-        </div>
-      </div>
-    </JoinWrap>
+        </LinkCardWrapper>
+      </LinkWrapper>
+    </HomeContainer>
   );
 }
 
-const JoinWrap = Styled.div`
+const HomeContainer = Styled.div`
   display : flex;
   flex-direction : column;
   align-items : center;
@@ -318,226 +356,6 @@ const JoinWrap = Styled.div`
     margin : 0;
     padding : 0;
   }
-  /* @keyframes moveUp {
-    from {
-      transform: translateY(8%);
-      opacity: 0;
-    }
-    to {
-      transform: translateY(0);
-      opacity: 1;
-    }
-  } */
-  .linkTo{
-    width : 100%;
-    padding-top : 208px;
-    margin-top : 150px;
-    padding-bottom : 187px;
-    background-color : #2f3438;
-    display : flex;
-    justify-content : center;
-    align-items : center;
-    gap : 110px;
-    &__container--card{
-      display : flex;
-      flex-direction : column;
-    }
-    &__title{
-      color : #ffffff;
-      height : 68px;
-    }
-    &__exp{
-      color : #dfdfdf;
-      margin : 31px 0;
-    }
-   &__button{
-    width: 200px;
-    height: 60px;
-    background-color : #36c8f5;
-    color : #ffffff;
-   }
-  }
-  .o2badge{
-    padding-top : 150px;
-    display : flex;
-    flex-direction: column;
-    align-items : center;
-    &__title{
-      color : #0d0d0d;
-      margin-bottom : 20px;
-    }
-    &__subtitle{
-      color : #6f6f6f;
-      margin-bottom : 150px;
-    }
-    &__container--badge{
-      display : grid;
-      grid-template-columns: repeat(2, 1fr);
-      grid-column-gap: 468px;
-      grid-row-gap: 100px;
-    }
-  }
-  .o2speaker{
-    width : 100vw;
-    background-color : #2f3438;
-    display : flex;
-    flex-direction : column;
-    align-items : center;
-    padding : 120px 0;
-    &__title{
-      color : #ffffff;
-    }
-    &__subtitle{
-      color : #a5a5a5;
-      margin-top : 20px;
-      margin-bottom : 138px;
-    }
-    &__exp{
-      color : #ffffff;
-      text-align: center;
-      margin-top : 48px;
-    }
-    &__container{
-      &--outer{
-        width : 1100px;
-        display : flex;
-        flex-direction : row;
-        justify-content : space-between;
-      }
-      &--inner{
-        display : flex;
-        flex-direction : column;
-        align-items : center;
-      }
-    }
-    &__image--badge{
-      width: 260px;
-      height: 260px;
-    }
-  }
-  .top{
-    padding-top : 178px;
-    width : 1109px;
-    /* animation-name: moveUp;
-    animation-duration: 1.5s; */
-    &__title{
-      &--O2{
-        font-size: 90px;
-        font-weight: bold;
-        line-height: 0.62;
-        letter-spacing: -0.5px;
-        font-family: HomepageBaukasten;
-        margin-bottom : 50px;
-        color : #0d0d0d;
-      }
-      &--subtitle{
-        color : #0d0d0d;
-      }
-    }
-    &__exp{
-      margin-top : 20px;
-      margin-bottom : 30px;
-      color : #3d3d3d;
-    }
-    &__button{
-      width : 164px;
-      height : 40px;
-      color : #ffffff;
-      background-color : #3d3d3d;
-      border-radius: 50px;
-    }
-  }
-  .middle{
-    &__title{
-      &--title{
-        color : #0d0d0d;
-      }
-      &--subtitle{
-        color : #6f6f6f;
-        margin-top : 20px;
-        margin-bottom : 40px;
-      }
-    }
-    &__exp{
-      color : #8b8b8b;
-    }
-  }
-  .learnMyself{
-    padding-top : 435px;
-    width : 1109px;
-    &__container--card{
-      margin-top :80px;
-      display : flex;
-      width : 1109px;
-      justify-content : space-between;
-    }
-    &__img--card{
-      width : 254px;
-      height : 259px;
-      width: 254px;
-      height: 259px;
-    }
-  }
-  .shareTogether{
-    width : 1109px;
-    padding-top : 270px;
-    padding-bottom : 151px;
-    &__title{
-      color : #3d3d3d;
-      margin-top : 10px;
-    }
-    &__container{
-      &--photo1{
-        margin-top : 90px;
-        display : flex;
-        flex-direction : row;
-      }
-      &--photo2{
-        margin-top : 153px;
-        display : flex;
-        flex-direction : row;
-        justify-content : space-between;
-      }
-      &--exp1{
-        display : flex;
-        flex-direction: column;
-        align-items : flex-start;
-        padding-top : 58px;
-      }
-      &--exp2{
-        display : flex;
-        flex-direction: column;
-        align-items : flex-start;
-        padding-top : 141px;
-      }
-    }
-    &__image1{
-      width : 672px;
-      height : 449px;
-      margin-right : 93px;
-    }
-    &__image2{
-      width: 464px;
-      height: 745px;
-    }
-    &__exp{
-      &--type{
-        color : #8b8b8b;
-      }
-    }
-    &__content{
-      margin-top : 32px;
-      font-family: AppleSDGothicNeo;
-      font-size: 14px;
-      font-weight: normal;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: 1.79;
-      letter-spacing: -0.5px;
-      color : #555555;
-    }
-  }
-
 `;
 
 export default Home;
