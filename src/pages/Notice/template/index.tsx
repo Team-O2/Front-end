bf50faa1e5035d9e6a128ce6e529cdf9a9fac1d4
JiyoungHyun <shyunji023@gmail.com>
2021-08-07 16:40:08 +0900

    Style: Notice Page, Template분리

--- src/pages/Notice/template/index.tsx
+++ src/pages/Notice/template/index.tsx
@@ -1,7 +1,46 @@
+import { NoticeHeader, NoticeList, SearchForm } from 'components/organisms';
 import React from 'react';
+import { INotice } from 'types/notice.type';
+import { NoticeWrapper } from './style';
 
-function Notice(): React.ReactElement {
-  return <div>Notice</div>;
+interface IProps {
+  reRenderKeyword: (keyword: string) => void;
+  keywordChange: () => void;
+  noticeList: Array<INotice> | null;
+  totalNoticeNum: number;
+  currentPage: number;
+  setCurrentPage: (value: number) => void;
 }
 
-export default Notice;
+function NoticeTemplate({
+  reRenderKeyword,
+  keywordChange,
+  totalNoticeNum,
+  noticeList,
+  currentPage,
+  setCurrentPage,
+}: IProps): React.ReactElement {
+  return (
+    <>
+      <NoticeHeader />
+      <NoticeWrapper>
+        <SearchForm
+          reRenderKeyword={reRenderKeyword}
+          keywordChange={keywordChange}
+          contentListNum={totalNoticeNum}
+          selectedCategory="공지사항"
+        ></SearchForm>
+        {noticeList && (
+          <NoticeList
+            noticeList={noticeList}
+            totalNoticeNum={totalNoticeNum}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          ></NoticeList>
+        )}
+      </NoticeWrapper>
+    </>
+  );
+}
+
+export default NoticeTemplate;
