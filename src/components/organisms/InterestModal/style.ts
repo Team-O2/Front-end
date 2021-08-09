b64a606dc19eda468eb291c3d02aa14850d0754f
Subin <sbsoobin@naver.com>
2021-08-09 23:30:49 +0900

    Refactor: 관심분야모달 모듈화

--- src/components/organisms/InterestModal/style.ts
+++ src/components/organisms/InterestModal/style.ts
@@ -99,35 +99,3 @@ export const ModalWrapper = Styled.div`
   flex-direction : column;
   align-items : center;
 `;
-
-export const JoinLabel = Styled(Label)`
-  font-size: 40px;
-  font-weight: bold;
-  margin-bottom : 20px;
-`;
-
-export const JoinButton = Styled(Button)<{ isButtonDisabled: boolean }>`
-  width: 406px;
-  height: 60px;
-  border-radius: 4px;
-  font-size : 16px;
-  font-weight : bold;     
-  color : ${palette('grayscale', 0)}; 
-  margin-top : 60px;
-   ${(props) =>
-     props.isButtonDisabled
-       ? css`
-           background-color: ${palette('grayscale', 1)};
-         `
-       : css`
-           background-image: linear-gradient(to right, ${palette('primary', 3)}, ${palette('primary', 0)});
-         `};
-`;
-
-export const JoinWrap = Styled.div`
-  display : flex;
-  flex-direction : column;
-  align-items : center;
-  margin-top : 100px;
-  margin-bottom : 250px;
-`;
