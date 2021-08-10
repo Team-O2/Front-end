d6eca23519b1d94c833bc92630756cbee0011e96
YeongWoooo <rdd9223@gmail.com>
2021-08-10 21:44:32 +0900

    Style: Setting 스타일 import 방식 변경

--- src/pages/Setting/template/index.tsx
+++ src/pages/Setting/template/index.tsx
@@ -4,25 +4,7 @@ import { DropDown, DropDownMulti, RemovableTag, ToggleSwitch } from 'components/
 import React from 'react';
 import { interestList } from 'resources/string';
 import { ISettingUserInfo } from 'types/setting.type';
-import {
-  Btn,
-  CategoryContainer,
-  Container,
-  DropdownWrapper,
-  EditBtn,
-  Email,
-  InterestList,
-  NicknameInputContainer,
-  PolicyCntnr,
-  ProfileContainer,
-  ProfileImg,
-  ProfileInput,
-  PwBtn,
-  Text,
-  TextWrapper,
-  Txt,
-  TxtSmall,
-} from './style';
+import * as S from './style';
 
 export interface IProps {
   userInfo: ISettingUserInfo;
@@ -52,24 +34,24 @@ function SettingTemplate({
   handlePasswordBtnClicked,
 }: IProps): React.ReactElement {
   return (
-    <Container>
-      <ProfileContainer>
-        <ProfileImg url={userInfo.img} />
+    <S.Container>
+      <S.ProfileContainer>
+        <S.ProfileImg url={userInfo.img} />
         <label htmlFor="file">
-          <EditBtn src={EditWhiteIcon} />
+          <S.EditBtn src={EditWhiteIcon} />
         </label>
-        <ProfileInput
+        <S.ProfileInput
           type="file"
           id="file"
           accept="image/jpeg, image/jpg, image/png"
           ref={imgInput}
           onChange={handleInputImg}
         />
-      </ProfileContainer>
-      <Txt>아이디</Txt>
-      <Email>{userInfo.email}</Email>
-      <Txt>닉네임</Txt>
-      <NicknameInputContainer>
+      </S.ProfileContainer>
+      <S.Txt>아이디</S.Txt>
+      <S.Email>{userInfo.email}</S.Email>
+      <S.Txt>닉네임</S.Txt>
+      <S.NicknameInputContainer>
         <StyledInput
           width="844px"
           height="60px"
@@ -78,23 +60,23 @@ function SettingTemplate({
           isConditionMet={userInfo.nickname !== ''}
           errorMsg="닉네임을 입력해주세요"
         />
-      </NicknameInputContainer>
-      <Txt>카테고리</Txt>
-      <CategoryContainer>
+      </S.NicknameInputContainer>
+      <S.Txt>카테고리</S.Txt>
+      <S.CategoryContainer>
         <DropDownMulti
           state={userInfo.interest}
           setState={handleUserInterest}
           defaultMsg="카테고리를 선택해 주세요 (최대 5개)"
           itemList={interestList}
         />
-      </CategoryContainer>
-      <InterestList>
+      </S.CategoryContainer>
+      <S.InterestList>
         {userInfo.interest.map((item: string, idx: number) => {
           return <RemovableTag text={item} setList={handleClickDel} key={idx} />;
         })}
-      </InterestList>
-      <Txt>성별</Txt>
-      <DropdownWrapper>
+      </S.InterestList>
+      <S.Txt>성별</S.Txt>
+      <S.DropdownWrapper>
         <DropDown
           state={userInfo.gender === 0 ? '남성' : userInfo.gender === 1 ? '여성' : '선택안함'}
           setState={handleUserGender}
@@ -103,20 +85,20 @@ function SettingTemplate({
           page="joinform"
           isSetting={true}
         />
-      </DropdownWrapper>
-      <Txt>비밀번호 변경</Txt>
-      <PwBtn onClick={handlePasswordBtnClicked}>비밀번호 변경하기</PwBtn>
-      <PolicyCntnr>
-        <TextWrapper>
-          <Text>이메일 수신 설정</Text>
-          <TxtSmall>오투에서 진행하는 챌린지, 이벤트, 프로모션에 관한 광고를 수신하겠습니다.</TxtSmall>
-        </TextWrapper>
+      </S.DropdownWrapper>
+      <S.Txt>비밀번호 변경</S.Txt>
+      <S.PwBtn onClick={handlePasswordBtnClicked}>비밀번호 변경하기</S.PwBtn>
+      <S.PolicyCntnr>
+        <S.TextWrapper>
+          <S.Text>이메일 수신 설정</S.Text>
+          <S.TxtSmall>오투에서 진행하는 챌린지, 이벤트, 프로모션에 관한 광고를 수신하겠습니다.</S.TxtSmall>
+        </S.TextWrapper>
         <ToggleSwitch checked={userInfo.marpolicy} onChange={handleUserMarpolicy} />
-      </PolicyCntnr>
-      <Btn isActive={isBtnAtv} onClick={handleClickEdit}>
+      </S.PolicyCntnr>
+      <S.Btn isActive={isBtnAtv} onClick={handleClickEdit}>
         수정완료
-      </Btn>
-    </Container>
+      </S.Btn>
+    </S.Container>
   );
 }
 
