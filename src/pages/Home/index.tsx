92f232626b77a28ebfeed435668fbed5e68b7aa6
Subin <sbsoobin@naver.com>
2021-08-12 15:38:16 +0900

    Refactor: 메인 애니메이션 제거

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { keyframes } from '@emotion/react';
 import { getGeneration } from 'apis';
 import {
   CommunicationKingFillIcon,
@@ -22,8 +21,8 @@ import {
 } from 'assets/images';
 import { BadgeCard, SpeakerExpCard } from 'components/molecules';
 import React, { useEffect, useState } from 'react';
-import Reveal from 'react-awesome-reveal';
 import { Link } from 'react-router-dom';
+import { keyframes } from 'styled-components';
 import {
   BadgeWrapper,
   ChallengeBtn,
@@ -86,30 +85,25 @@ function Home(): React.ReactElement {
 
   return (
     <HomeContainer image={LandingBackImg}>
-      <Reveal keyframes={moveUp} cascade>
-        <TopWrapper>
-          <O2Title>O2</O2Title>
-          <TopSubTitle>
-            함께 성장하는 공간, <br />
-            창업가들이 마시는 산소
-          </TopSubTitle>
-          <TopExp>답답한 창업 여정에 시원한 산소 한 모금</TopExp>
-          <Link to={`/challenge/${generationNum}`}>
-            <ChallengeBtn>챌린지 보러가기</ChallengeBtn>
-          </Link>
-        </TopWrapper>
-      </Reveal>
+      <TopWrapper>
+        <O2Title>O2</O2Title>
+        <TopSubTitle>
+          함께 성장하는 공간, <br />
+          창업가들이 마시는 산소
+        </TopSubTitle>
+        <TopExp>답답한 창업 여정에 시원한 산소 한 모금</TopExp>
+        <Link to={`/challenge/${generationNum}`}>
+          <ChallengeBtn>챌린지 보러가기</ChallengeBtn>
+        </Link>
+      </TopWrapper>
       <ChallengeWrapper>
-        <Reveal keyframes={moveUp}>
-          <MiddleTitle>Learn Myself</MiddleTitle>
-          <MiddleSubTitle>나에게 배우다, 나를 배우다.</MiddleSubTitle>
-          <MiddleExp>
-            오늘의 잘한 점과 못한 점, 배운 점을 기록해요.
-            <br />
-            배운 점을 적용하며 더 성장한 내일의 나를 만들어요.
-          </MiddleExp>
-        </Reveal>
-        {/* <Reveal cascade damping={0.3} keyframes={moveUp} triggerOnce> */}
+        <MiddleTitle>Learn Myself</MiddleTitle>
+        <MiddleSubTitle>나에게 배우다, 나를 배우다.</MiddleSubTitle>
+        <MiddleExp>
+          오늘의 잘한 점과 못한 점, 배운 점을 기록해요.
+          <br />
+          배운 점을 적용하며 더 성장한 내일의 나를 만들어요.
+        </MiddleExp>
         <ChallengeCardWrapper>
           <ChallengeCard src={MainCard1Img} />
           <ChallengeCard src={MainCard2Img} />
@@ -119,62 +113,54 @@ function Home(): React.ReactElement {
         {/* </Reveal> */}
       </ChallengeWrapper>
       <ConcertWrapper>
-        <Reveal keyframes={moveUp}>
-          <MiddleTitle>Share Together</MiddleTitle>
-          <MiddleSubTitle>경험을 나누다, 함께 성장하다.</MiddleSubTitle>
-          <MiddleExp>
-            누구나 O2 Speaker가 될 수 있어요.
-            <br />
-            O2 People 앞에서 당신의 이야기를 나눠주세요.
-          </MiddleExp>
-        </Reveal>
+        <MiddleTitle>Share Together</MiddleTitle>
+        <MiddleSubTitle>경험을 나누다, 함께 성장하다.</MiddleSubTitle>
+        <MiddleExp>
+          누구나 O2 Speaker가 될 수 있어요.
+          <br />
+          O2 People 앞에서 당신의 이야기를 나눠주세요.
+        </MiddleExp>
         <ConcertWrapper1>
           <ConcertImage1 src={Concert1Img}></ConcertImage1>
           <ConcertExpContainer1>
-            <Reveal keyframes={moveUp}>
-              <ConcertExpType>Share Together 인기글</ConcertExpType>
-              <ConcertTitle>
-                내가 힘들 때 버틸 수 있었던 <br />
-                3가지
-              </ConcertTitle>
-              <ConcertContent>
-                창업을 하다보면 정말 생각하지 못했던 어려운 일들이 <br />
-                많이 일어나는건 누구나 알고계실거라 생각합니다. <br />
-                중간에 포기하고 싶어지는 순간에 앵엘님의
-                <br />
-                [내가 힘들 때 버틸 수 있었던 3가지] 강연으로 나만의 <br />
-                버팀목도 한번 만들어보세요.
-              </ConcertContent>
-            </Reveal>
+            <ConcertExpType>Share Together 인기글</ConcertExpType>
+            <ConcertTitle>
+              내가 힘들 때 버틸 수 있었던 <br />
+              3가지
+            </ConcertTitle>
+            <ConcertContent>
+              창업을 하다보면 정말 생각하지 못했던 어려운 일들이 <br />
+              많이 일어나는건 누구나 알고계실거라 생각합니다. <br />
+              중간에 포기하고 싶어지는 순간에 앵엘님의
+              <br />
+              [내가 힘들 때 버틸 수 있었던 3가지] 강연으로 나만의 <br />
+              버팀목도 한번 만들어보세요.
+            </ConcertContent>
           </ConcertExpContainer1>
         </ConcertWrapper1>
         <ConcertWrapper2>
           <ConcertExpContainer2>
-            <Reveal keyframes={moveUp}>
-              <ConcertExpType>Share Together 인기글</ConcertExpType>
-              <ConcertTitle>
-                CEO라는 꿈. <br />
-                정말 나를 위한 유일한 길일까?
-              </ConcertTitle>
-              <ConcertContent>
-                여러분은 자신의 꿈에 얼마나 큰 확신을 가지고있나요? <br />
-                아마 꿈을 향해 나아가면서도 끊임없이 ‘내가 잘 하고 있는 것인가?‘, <br />
-                ‘이게 정말 나를 위한 길인가?’ 이런 생각이 드실거라 생각합니다. <br />
-                깡토님의 [CEO라는 꿈. 정말 나를 위한 유일한 길일까?] 강연에서는 불안
-                <br />
-                하기만 CEO라는 꿈을 어떻게 이룰 수 있었는지, <br />
-                깡토님의 실제 스토리를 여러분들께 들려드립니다.
-              </ConcertContent>
-            </Reveal>
+            <ConcertExpType>Share Together 인기글</ConcertExpType>
+            <ConcertTitle>
+              CEO라는 꿈. <br />
+              정말 나를 위한 유일한 길일까?
+            </ConcertTitle>
+            <ConcertContent>
+              여러분은 자신의 꿈에 얼마나 큰 확신을 가지고있나요? <br />
+              아마 꿈을 향해 나아가면서도 끊임없이 ‘내가 잘 하고 있는 것인가?‘, <br />
+              ‘이게 정말 나를 위한 길인가?’ 이런 생각이 드실거라 생각합니다. <br />
+              깡토님의 [CEO라는 꿈. 정말 나를 위한 유일한 길일까?] 강연에서는 불안
+              <br />
+              하기만 CEO라는 꿈을 어떻게 이룰 수 있었는지, <br />
+              깡토님의 실제 스토리를 여러분들께 들려드립니다.
+            </ConcertContent>
           </ConcertExpContainer2>
           <ConcertImage2 src={Concert2Img}></ConcertImage2>
         </ConcertWrapper2>
       </ConcertWrapper>
       <O2SpeakerWrapper>
-        <Reveal keyframes={moveUp}>
-          <O2SpeakerTitle>To Be an O2 Speaker</O2SpeakerTitle>
-          <O2SpeakerSubTitle>뱃지를 모아 O2 Speaker가 되어보세요!</O2SpeakerSubTitle>
-        </Reveal>
+        <O2SpeakerTitle>To Be an O2 Speaker</O2SpeakerTitle>
+        <O2SpeakerSubTitle>뱃지를 모아 O2 Speaker가 되어보세요!</O2SpeakerSubTitle>
         {/* <Reveal cascade damping={0.5} keyframes={moveUp} triggerOnce> */}
         <O2SpeakerCardWrapper>
           <SpeakerExpCard img={SpeakerBadge1Img} exp1="챌린지 개수를 주당" exp2="3개 이상으로 설정" />
@@ -184,10 +170,8 @@ function Home(): React.ReactElement {
         {/* </Reveal> */}
       </O2SpeakerWrapper>
       <O2BadgeWrapper>
-        <Reveal keyframes={moveUp}>
-          <O2BadgeTitle>O2 Badge</O2BadgeTitle>
-          <O2BadgeSubTitle>열심히 활동하면 멋진 오투의 뱃지들을 받을 수 있어요.</O2BadgeSubTitle>
-        </Reveal>
+        <O2BadgeTitle>O2 Badge</O2BadgeTitle>
+        <O2BadgeSubTitle>열심히 활동하면 멋진 오투의 뱃지들을 받을 수 있어요.</O2BadgeSubTitle>
         {/* <Reveal cascade damping={0.2} keyframes={moveUp} triggerOnce> */}
         <BadgeWrapper>
           <BadgeCard
@@ -285,9 +269,7 @@ function Home(): React.ReactElement {
             참여해 보세요!
           </LinkExp>
           <Link to="/challenge/register">
-            <Reveal keyframes={moveUp}>
-              <LinkBtn>More</LinkBtn>
-            </Reveal>
+            <LinkBtn>More</LinkBtn>
           </Link>
         </LinkCardWrapper>
         <LinkCardWrapper>
@@ -297,11 +279,9 @@ function Home(): React.ReactElement {
             <br />
             챌린지를 시작해 보세요
           </LinkExp>
-          <Reveal keyframes={moveUp}>
-            <Link to={`/challenge/${generationNum}`}>
-              <LinkBtn>More</LinkBtn>
-            </Link>
-          </Reveal>
+          <Link to={`/challenge/${generationNum}`}>
+            <LinkBtn>More</LinkBtn>
+          </Link>
         </LinkCardWrapper>
         <LinkCardWrapper>
           <LinkTitle>Share Together</LinkTitle>
@@ -310,11 +290,9 @@ function Home(): React.ReactElement {
             <br />
             들어보세요!
           </LinkExp>
-          <Reveal keyframes={moveUp}>
-            <Link to="/concert">
-              <LinkBtn>More</LinkBtn>
-            </Link>
-          </Reveal>
+          <Link to="/concert">
+            <LinkBtn>More</LinkBtn>
+          </Link>
         </LinkCardWrapper>
       </LinkWrapper>
     </HomeContainer>
