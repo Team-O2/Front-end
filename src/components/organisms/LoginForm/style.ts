b3b5223e3278dbdb9291c92dca29482d506eb0f8
Subin <sbsoobin@naver.com>
2021-08-13 10:15:11 +0900

    Refactor: 사용하지 않는 Import 제거

--- src/components/organisms/LoginForm/style.ts
+++ src/components/organisms/LoginForm/style.ts
@@ -1,6 +1,4 @@
-import { Button } from 'components/atoms';
 import Styled from 'styled-components';
-import { palette } from 'styled-tools';
 
 export const LoginformWrap = Styled.div`
   display : flex;
