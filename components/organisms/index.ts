0205ab5d0de65249c09a04f2fd942e38075f0f61
JiyoungHyun <shyunji023@gmail.com>
2021-08-15 15:55:54 +0900

    Refactor: Footer 이동

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,3 +1,4 @@
+export { default as Footer } from './Footer';
 export { default as NoticeHeader } from './NoticeHeader';
 export { default as NoticeList } from './NoticeList';
 export { default as SearchForm } from './SearchForm';
