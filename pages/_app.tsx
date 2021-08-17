3efae448f57c5ef20e8424d688ae8bc776495c3d
YeongWoooo <rdd9223@gmail.com>
2021-08-17 13:51:27 +0900

    Refactor: 메타데이터 관심사 분리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { Footer } from 'components/organisms';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/GlobalStyles';
@@ -9,22 +8,13 @@ import '../public/assets/fonts/index.css';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>O2</title>
-      </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <RecoilRoot>
-          <Component {...pageProps} />
-          <Footer />
-        </RecoilRoot>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RecoilRoot>
+        <Component {...pageProps} />
+        <Footer />
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>O2</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
