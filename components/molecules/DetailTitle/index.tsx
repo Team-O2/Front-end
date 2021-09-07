32fded86d60af931cbd30057c858efaf9e2b5fb6
JiyoungHyun <shyunji023@gmail.com>
2021-09-07 21:48:23 +0900

    Refactor: qa반영 시간 표현 변경

--- components/molecules/DetailTitle/index.tsx
+++ components/molecules/DetailTitle/index.tsx
@@ -12,6 +12,28 @@ interface IProps {
 
 function DetailTitle({ pageName, title, authorNickname, createdAt, interestList }: IProps): React.ReactElement {
   const interestDivide = interestList?.join(' | ');
+
+  const timeForToday = (value: string) => {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const uploadTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+    if (uploadTime < 1) {
+      return '방금전';
+    } else if (uploadTime < 60) {
+      return `${uploadTime}분전`;
+    }
+
+    const uploadTimeHour = Math.floor(uploadTime / 60);
+    if (uploadTimeHour < 24) {
+      return `${uploadTimeHour}시간전`;
+    }
+
+    const uploadTimeDay = Math.floor(uploadTimeHour / 60 / 24);
+    if (uploadTimeDay < 365) {
+      return dayjs(createdAt).format('YY.MM.DD');
+    }
+  };
   return (
     <>
       <DetailTitleWrapper>
@@ -23,7 +45,7 @@ function DetailTitle({ pageName, title, authorNickname, createdAt, interestList 
         </Middle>
         <Bottom>
           <p>{authorNickname}</p>
-          <p>{`${dayjs(createdAt).format('YY.MM.DD')}`}</p>
+          <p>{timeForToday(createdAt)}</p>
           <p>{interestDivide}</p>
         </Bottom>
       </DetailTitleWrapper>
