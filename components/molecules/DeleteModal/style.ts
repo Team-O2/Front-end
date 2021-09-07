f00fed2e9f9e63dd296b103b1507110c4ca70487
YeongWoooo <rdd9223@gmail.com>
2021-09-07 19:27:23 +0900

    Fix: 피그마와 UI가 다른부분 해결

--- components/molecules/DeleteModal/style.ts
+++ components/molecules/DeleteModal/style.ts
@@ -2,39 +2,41 @@ import Styled from 'styled-components';
 import { palette, theme } from 'styled-tools';
 
 export const NoticeWrapper = Styled.div`
-  padding: 0px 80px 0px 80px;
-
-  img {
-    display:flex;
-    margin:auto;
-    margin-top:-40px;
-  }
+  display: flex;
+  flex-direction: column;
 
   h1 {
-    ${theme('font.dp4')};
-    padding:20px 0px 20px 0px;
+    ${theme('font.h1')};
     text-align: center;
     color: ${theme('grayscale', 9)};
+    margin-bottom: 8px;
   }
 
   p {
-    ${theme('font.body3')};
+    ${theme('font.body2')};
     text-align: center;
     color: ${theme('grayscale', 9)};
   }
 `;
 
+export const ImgWrapper = Styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: -30px;
+  margin-bottom: 12px;
+`;
+
 export const ButtonWrapper = Styled.div`
-  padding-top:50px;
+  padding-top: 32px;
   text-align: center;
   
   button:first-child {
-    ${theme('font.subhead4')};
+    ${theme('font.subhead3')};
     color: ${palette('grayscale', 2)};
   }
 
   button:nth-child(2) {
-    ${theme('font.subhead4')};
+    ${theme('font.subhead3')};
     padding-left:170px;
     color: ${palette('primary', 7)};
   }
@@ -48,8 +50,8 @@ export const Wrapper = Styled.div`
   left:0;
   margin:auto;
   border-radius: 16px;
-  border: solid 1px ${palette('grayscale', 4)};
+  border: solid 1px ${palette('primary', 7)};
   background-color: ${palette('grayscale', 0)};
-  width: 500px;
-  height: 312px;
+  width: 330px;
+  height: 200px;
 `;
