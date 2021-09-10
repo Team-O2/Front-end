0289134da8e59cd1977e8fad19430136cc25c261
Hyoin-kim <hi980506@naver.com>
2021-09-11 03:41:08 +0900

    Refactor : 댓글 css 디자인 수정

--- components/molecules/ChallengeCommentWrite/style.ts
+++ components/molecules/ChallengeCommentWrite/style.ts
@@ -1,15 +1,14 @@
 import { Button } from 'components/atoms';
 import Styled from 'styled-components';
-import { palette, theme } from 'styled-tools';
+import { theme } from 'styled-tools';
 
 export const CommentSubmitButton = Styled(Button)`
     color: #555555;
     border:none;
-    background-color: ${palette('grayscale', 0)};
     ${theme('font.subhead3')};
 `;
 
-export const CommentWriteWrapper = Styled.div<{ isCommentt: boolean }>`
+export const CommentWriteWrapper = Styled.div<{ isCommentt: boolean }>`    
     form{
       display: flex;
       flex-direction: column;
@@ -17,9 +16,9 @@ export const CommentWriteWrapper = Styled.div<{ isCommentt: boolean }>`
     }
 
     textarea {
-      width: ${({ isCommentt }) => (isCommentt ? '724px' : '591px')};
+      width: ${({ isCommentt }) => (isCommentt ? '724px' : '575px')};
       height: ${({ isCommentt }) => (isCommentt ? '110px' : '54px')};
-      margin: 0 0 8px;
+      margin: 20px 0 10px 8px;
       padding: 15px 15px;
       opacity: 0.5;
       align-items:center;
