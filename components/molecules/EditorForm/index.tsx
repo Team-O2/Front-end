73fb300ece22c99e6c391b1b80d3be53af79a586
Subin <sbsoobin@naver.com>
2021-09-12 16:40:28 +0900

    Fix: Quill next에서 안되는 문제 해결

--- components/molecules/EditorForm/index.tsx
+++ components/molecules/EditorForm/index.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import React from 'react';
 import 'react-quill/dist/quill.snow.css';
 
@@ -7,42 +8,42 @@ export interface IProps {
   setContent: (value: string) => void;
 }
 
-function EditorForm({ content, setContent }: IProps): React.ReactElement {
-  const ReactQuill = typeof window === 'object' ? require('react-quill') : () => false;
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
 
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      //   ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      ['underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-      [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
-      ['clean'],
-    ],
-  };
-  const formats = [
-    //'font',
-    'header',
-    // 'bold',
-    // 'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-    'align',
-    'color',
-    'background',
-  ];
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+    [{ align: [] }, { color: [] }, { background: [] }],
+    ['clean'],
+  ],
+};
+const formats = [
+  'header',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'align',
+  'color',
+  'background',
+];
 
+function EditorForm({ content, setContent }: IProps) {
   return (
-    <ReactQuill
-      theme="snow"
+    <QuillNoSSRWrapper
+      style={{ height: '700px', width: '844px', marginBottom: '80px', marginTop: '34px' }}
       modules={modules}
       formats={formats}
+      theme="snow"
       value={content || ''}
       onChange={(content: string) => {
         setContent(content);
