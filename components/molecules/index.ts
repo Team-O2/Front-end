71e69352749975b2f0c497369bc04c56ca2290e2
Subin <sbsoobin@naver.com>
2021-09-12 18:41:38 +0900

    Feat: 랜딩페이지 로티 적용

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -31,6 +31,7 @@ export { default as FindPwdForm } from './FindPwdForm';
 export { default as FindPwdModal } from './FindPwdModal';
 export { default as FormInput } from './FormInput';
 export { default as HamDropDown } from './HamDropDown';
+export { default as LandingLottie } from './LandingLottie';
 export { default as LoginModal } from './LoginModal';
 export { default as MyPageConcertCard } from './MyPageConcertCard';
 export { default as MyPageSection } from './MyPageSection';
