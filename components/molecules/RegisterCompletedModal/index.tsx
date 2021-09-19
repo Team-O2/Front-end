7d5dca24f79109742e902962969aea16b074fb27
Hyoin-Kim <hi980506@naver.com>
2021-09-19 23:44:53 +0900

    Refactor : 챌린지 신청하기 모달 확인 팝업 추가

--- components/molecules/RegisterCompletedModal/index.tsx
+++ components/molecules/RegisterCompletedModal/index.tsx
@@ -1,10 +1,9 @@
-import { Modal } from 'components/atoms';
-import Image from 'next/image';
+import { Button, Icon, Modal } from 'components/atoms';
 import { CheckCircleIcon } from 'public/assets/images';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { userState } from 'stores/user';
-import { RegisterSubmitWrapper } from './style';
+import { ButtonWrapper, ImgWrapper, RegisterSubmitWrapper } from './style';
 
 interface IProps {
   isRegisterSubmit: boolean;
@@ -13,13 +12,21 @@ interface IProps {
 
 function RegisterCompletedModal({ isRegisterSubmit, setIsRegisterSubmit }: IProps): React.ReactElement {
   const userData = useRecoilValue(userState);
+  const handleConfirmButton = () => {
+    setIsRegisterSubmit(false);
+  };
   return (
     <Modal isOpen={isRegisterSubmit} setIsOpen={setIsRegisterSubmit} isBlur={true}>
       <RegisterSubmitWrapper>
-        <Image src={CheckCircleIcon} alt=""></Image>
+        <ImgWrapper>
+          <Icon src={CheckCircleIcon} alt=""></Icon>
+        </ImgWrapper>
         <h5>신청완료!</h5>
         <p>{userData?.nickname}님 챌린지가 정상적으로</p>
         <p> 신청되었습니다.</p>
+        <ButtonWrapper>
+          <Button onClick={handleConfirmButton}>확인</Button>
+        </ButtonWrapper>
       </RegisterSubmitWrapper>
     </Modal>
   );
