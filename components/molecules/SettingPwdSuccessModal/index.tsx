cc933cc5ecd4e9b89fd2f856fee5c234abf27d46
Hyoin-Kim <hi980506@naver.com>
2021-09-19 23:45:06 +0900

    Refactor : 패스워드 변경 완료 모달 확인 팝업 추가

--- components/molecules/SettingPwdSuccessModal/index.tsx
+++ components/molecules/SettingPwdSuccessModal/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from 'components/atoms';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { CheckedIcon } from 'public/assets/images';
 import React from 'react';
 import * as S from './style';
+import { ButtonWrapper } from './style';
 
 interface IProps {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -27,6 +29,9 @@ function SettingPwdSuccessModal({ setIsOpen, url }: IProps): React.ReactElement 
           </S.CheckIcon>
           <S.ColoredTxt>완료</S.ColoredTxt>
           <S.Message>비밀번호 변경이 완료되었습니다</S.Message>
+          <ButtonWrapper>
+            <Button onClick={handleOnClick}>확인</Button>
+          </ButtonWrapper>
         </S.InnerModalWrap>
       </S.ModalWrap>
     </>
