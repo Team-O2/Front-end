4b04d32fc00e0c4bbf418d712a2cd271b97add30
YeongWoooo <rdd9223@gmail.com>
2021-09-26 07:58:35 +0900

    Refactor: 바뀐 url, 빈 url 적용

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['o2-server.s3.ap-northeast-2.amazonaws.com'],
+    domains: ['o2-server.s3.ap-northeast-2.amazonaws.com', '', 'o2-server.s3.amazonaws.com'],
   },
 };
